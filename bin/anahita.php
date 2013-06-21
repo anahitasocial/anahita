68ce75c3d331986e22a8819f2eb856fe5f313734
Arash  Sanieyan <ash@peerglobe.com>
2013-06-20 20:58:26 -0700

    Merge branch 'master' of git://github.com/anahitasocial/anahita-new

--- bin/anahita.php
+++ bin/anahita.php
@@ -1,11 +1,24 @@
 <?php 
-print __FILE__;
-die;
-require_once 'vendor/autoload.php';
+
+set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__.'/../');
+
+$files = array(
+        __DIR__ . '/../vendor/autoload.php',
+        __DIR__ . '/../../../autoload.php'
+);
+foreach($files as $file) 
+{
+    if ( file_exists($file) ) 
+    {
+        require_once($file);
+        define('VENDOR_DIR', realpath(dirname($file)));
+        define('ROOT', realpath(dirname($file).'/../'));
+    }
+}
 
 require_once 'console/application.php';
 
-$console = new Console\Application(__DIR__,__DIR__.'/site');
+$console = new Console\Application(ROOT,ROOT.'/site');
 
 require_once 'console/commands/create.php';
 require_once 'console/commands/bundle.php';
