26c0a6c2766a142bb1f230e32add9ca7a22c7da8
Arash  Sanieyan <ash@peerglobe.com>
2013-06-26 19:37:48 -0700

    refactor the configuration task

--- bin/anahita.php
+++ bin/anahita.php
@@ -9,7 +9,7 @@ $files = array(
         __DIR__ . '/../../../autoload.php'
 );
 
-global $composerLoader;
+global $composerLoader, $console;
 
 foreach($files as $file) 
 {
@@ -22,26 +22,23 @@ foreach($files as $file)
     }
 }
 
-$composerLoader->add('Console\\', __DIR__.'../console');
+$composerLoader->add('Console\\', __DIR__.'/..');
 
 $console = new Console\Application(realpath(__DIR__.'/../'), COMPOSER_ROOT.'/www', array('Custom'=>COMPOSER_ROOT.'/packages'),COMPOSER_ROOT.'/config');
 
-require_once 'console/commands/create.php';
-require_once 'console/commands/package.php';
-require_once 'console/commands/migrate.php';
-
-//include all the tasks
-//for now it's good but later we should not do this
-//we should use autoload 
-if ( file_exists(COMPOSER_ROOT.'/tasks/') ) {
-    set_include_path(get_include_path() . PATH_SEPARATOR . COMPOSER_ROOT.'/tasks/');
-    $files = new \DirectoryIterator(COMPOSER_ROOT.'/tasks/');
+function include_tasks($directory) 
+{
+    global $console;    
+    $files = new \DirectoryIterator($directory);
     foreach($files as $file) {
         if ( strpos($file, '.task.php') ) {
             require_once $file->getPathName();
         }
     }
 }
+
+include_tasks(__DIR__.'/../console/tasks');
+include_tasks(COMPOSER_ROOT.'/tasks');
 
 $console->run();
 exit(0);
--- console/config.php
+++ console/config.php
@@ -1,239 +1,209 @@
 <?php 
 
-function install($src, $target, $configure = false)
+namespace Console;
+
+class Config
 {
-    $mapper = new \Installer\Mapper($src, $target);
-    $patterns = array(
-            '#^(site|administrator)/(components|modules|templates|media)/([^/]+)/.+#' => '\1/\2/\3',
-            '#^(components|modules|templates|libraries|media)/([^/]+)/.+#' => '\1/\2',
-            '#^(cli)/.+#'    => 'cli',
-            '#^plugins/([^/]+)/([^/]+)/.+#' => 'plugins/\1/\2',
-            '#^(administrator/)?(images)/.+#' => '\1\2',
-            '#^(site|administrator)/includes/.+#' => '\1/includes',
-            '#^(vendors|migration)/.+#'    => '',
-            '#^configuration\.php-dist#'   => '',
+    protected $_site_path;
+    
+    protected $_key_map = array(        
+        'database_type'      => 'dbtype',
+        'database_host'      => 'host',
+        'database_user'      => 'user',
+        'database_password'  => 'password',
+        'database_name'      => 'db',
+        'database_prefix'    => 'dbprefix',
+        'enable_debug'       => 'debug',
+        'enable_caching'     => 'caching',
+        'url_rewrite'        => 'sef_rewrite',
+        'cache_lifetime'     => 'cachetime',
+        'session_lifetime'   => 'lifetime',
+        'offline',                       
+        'secret',
+        'error_reporting',
+        'session_handler',
+        'cache_handler'
     );
     
-    if ( file_exists($target.'/configuration.php') && 
-            !$configure ) 
+    protected $_data = array();
+    
+    protected $_configuration_file;
+    
+    public function __construct($site_path)
     {
-        $patterns['#^installation/.+#'] = '';
-    }
-    
-    IO\write("Symlinking files...");
-    $mapper->addMap('vendor/mc/rt_missioncontrol_j15','administrator/templates/rt_missioncontrol_j15');
-    $mapper->addCrawlMap('vendor/joomla', $patterns);
-    $mapper->addCrawlMap('vendor/nooku',  $patterns);
-    $mapper->addCrawlMap('src/anahita',   $patterns);
-    $mapper->symlink();
-    $mapper->getMap('vendor/joomla/index.php','index.php')->copy();
-    
-    if ( file_exists($target.'/installation') ) {
-        $mapper->getMap('vendor/joomla/installation/index.php','installation/index.php')->copy();
-    }
-    
-    $mapper->getMap('vendor/joomla/administrator/index.php','administrator/index.php')->copy();    
-    
-    @mkdir($target.'/tmp',   0755);
-    @mkdir($target.'/cache', 0755);
-    @mkdir($target.'/log',   0755);
-    @mkdir($target.'/administrator/cache',   0755);    
-}
-
-function config($target)
-{
-    IO\write('Creating configuration file...');
-    define('DS', DIRECTORY_SEPARATOR);
-    define( '_JEXEC', 1 );
-    define('JPATH_BASE',           $target);
-    define('JPATH_ROOT',           JPATH_BASE );
-    define('JPATH_SITE',           JPATH_ROOT );
-    define('JPATH_CONFIGURATION',  JPATH_ROOT );
-    define('JPATH_ADMINISTRATOR',  JPATH_ROOT.'/administrator');
-    define('JPATH_XMLRPC',         JPATH_ROOT.'/xmlrpc');
-    define('JPATH_LIBRARIES',      JPATH_ROOT.'/libraries');
-    define('JPATH_PLUGINS',        JPATH_ROOT.'/plugins');
-    define('JPATH_INSTALLATION',   JPATH_ROOT.'/installation');
-    define('JPATH_THEMES',         JPATH_BASE.'/templates');
-    define('JPATH_CACHE',		   JPATH_BASE.'/cache' );
-    include_once (JPATH_LIBRARIES . '/joomla/import.php');
-    include_once (JPATH_BASE."/installation/installer/helper.php");
-    $database['name'] = IO\read('Enter Database Name: ', array('key'=>'db_name'));
-    $database['host'] = IO\read('Enter Database Host: ', array('key'=>'db_host','default'=>'127.0.0.1'));
-    $database['port'] = IO\read('Enter Database Port: ', array('key'=>'db_port','default'=>3306));
-    $database['user'] = IO\read('Enter Database User: ', array('key'=>'db_user'));
-    $database['password'] = IO\read('Enter Database Password: ', array('key'=>'db_password'));
-    $database['prefix']   = IO\read('Enter Database Prefix: ', array('key'=>'db_prefix','default'=>'jos')).'_';
-    IO\write('connecting to database...');
-    IO\write($database);
-    $errors		 = array();
-    $db = JInstallationHelper::getDBO('mysqli',$database['host'],$database['user'],$database['password'],$database['name'],$database['prefix'],false);
-    if ( $db instanceof JException )
-    {
-        IO\write(str_repeat('*', 80));
-        IO\write($db->toString());
-        IO\write(str_repeat('*', 80));
-        exit(1);
-    }
-    $db_exists = $db->select($database['name']);
-    if ( $db_exists )
-    {
-        $drop_db = IO\read('Database exists. Drop database ?', array('key'=>'drop_db','boolean'=>true,'default'=>'n'));
-        if ( $drop_db )
+        $this->_site_path = $site_path;
+        $map = array();
+        foreach($this->_key_map as $key => $value) 
         {
-            IO\write('dropping existing database...');
-            JInstallationHelper::deleteDatabase($db, $database['name'], $database['prefix'], $errors);
-            $db_exists = false;
+            if ( is_numeric($key) ) {
+                $key = $value;
+            }    
+            $map[$key] = $value;
         }
-    }
-    if ( !$db_exists )
-    {
-        IO\write('creating new database...');
-        JInstallationHelper::createDatabase($db, $database['name'],true);
-        $db->select($database['name']);
-    
-        $sql_files = array(JPATH_ROOT."/installation/sql/mysql/schema.sql",JPATH_ROOT."/installation/sql/mysql/install.sql");
-        IO\write('populating database...');
-        array_walk($sql_files, function($file) use($db) {
-            JInstallationHelper::populateDatabase($db, $file, $errors);
-        });
-        $admin_name = IO\read('Enter admin name: ', array('key'=>'admin_name','required'=>true));
-        $admin_passwd = IO\read('Enter admin password: ', array('key'=>'admin_password','required'=>true));
-        $admin_email  = IO\read('Enter admin email: ', array('key'=>'admin_email','required'=>true));
-        date_default_timezone_set('GMT');
-        $vars = array(
-                'DBhostname' => $database['host'],
-                'DBuserName' => $database['user'],
-                'DBpassword' => $database['password'],
-                'DBname' 	 => $database['name'],
-                'DBPrefix'   => $database['prefix'],
-                'adminName'  => $admin_name,
-                'adminEmail' => $admin_email,
-                'adminPassword' => $admin_passwd
+        $this->_key_map = $map;
+        $this->_data = array(
+            'debug_db'     => 0,
+            'debug_lang'   => 0,
+            'mailer'       => 'mail',
+            'mailfrom'     => '',
+            'sendmail'     => '/usr/sbin/sendmail',
+            'smtpauth'     => '0',
+            'smtpuser'     => '',
+            'smtppass'     => '',
+            'smtphost'     => 'localhost',
+            'force_ssl'    => 0,
+            'log_path'     => $site_path.'/log',
+            'tmp_path'     => $site_path.'/tmp',
+            'offline_message' => 'This site is down for maintenance.<br /> Please check back again soon.',
+            'sitename'        => 'Anahita',
+            'editor'          => 'tinymce'     
         );
-        if ( !JInstallationHelper::createAdminUser($vars) )
+        
+        $this->set(array(
+           'secret'           => '',
+           'offline'          => 0,
+           'enable_debug'     => 0,
+           'cache_duration'   => 15,
+           'session_duration' => 15,
+           'error_reporting' => 0,
+           'enable_caching'  => 1,
+           'url_rewrite'     => 0,
+           'session_handler'    => function_exists('apc_fetch') ? 'apc' : 'database',
+           'cache_handler'      => function_exists('apc_fetch') ? 'apc' : 'file'
+        ));
+        
+        $this->_configuration_file = $site_path.'/configuration.php'; 
+        if ( file_exists($this->_configuration_file) ) 
         {
-            IO\write(str_repeat('*', 80));
-            IO\write("Counldn't create an admin user. Make sure you have entered a correct email");
-            IO\write(str_repeat('*', 80));
-            exit(1);
+            $classname = 'JConfig'.md5(uniqid());            
+            $content   = file_get_contents($this->_configuration_file);
+            $content   = str_replace('JConfig', $classname, $content);
+            $content   = str_replace(array('<?php',''), '', $content);            
+            $classname = '\\'.$classname;
+            $return = eval($content);  
+            if ( class_exists($classname) ) 
+            {
+                $config = new $classname;
+                $this->_data = array_merge($this->_data, get_object_vars($config));                                
+            }                      
+        }
+        $this->database_type = 'mysqli';
+    }
+    
+    public function enableDebug()
+    {
+        $this->set(array(
+            'error_reporting' => E_ALL,
+            'enable_debug'    => 1,
+        ));    
+    }
+    
+    public function disableDebug()
+    {
+        $this->set(array(
+            'error_reporting' => 0,
+            'enable_debug'    => 0,
+        ));
+    }
+    
+    public function set($key ,$value = null)
+    {
+        if ( is_array($key) ) 
+        {
+            foreach($key as $k => $v) {
+                $this->$k = $v;
+            }
+        } else {
+            $this->$key = $value;
         }
     }
-    jimport('joomla.user.helper');
-    write_config(JPATH_BASE, array(
-    'site'          => JPATH_ROOT,
-    'db_host'       => $database['host'],
-    'db_username'   => $database['user'],
-    'db_password'   => $database['password'],
-    'db_name' 	    => $database['name'],
-    'db_prefix'     => $database['prefix'],
-    'secret'		=> JUserHelper::genRandomPassword(32)
-    ));
-    exec("rm -rf ".JPATH_ROOT."/installation");
-    IO\write(str_repeat('*', 80));
-    IO\write("Congradulation you're done.");
-    if ( !$db_exists )
-    {
-        IO\write("Please login-in with the following credentials");
-        IO\write(array(
-            'username' => 'admin',
-            'password' => $admin_passwd
-        ));
-    }    
-    return array(
-        'database' => $database        
-    );
-}
-
-function write_config($target, $config)
-{
-    extract($config);
-    $cache 			 = 'file';
-    $session_handler = 'database';
-    if ( function_exists('apc_exists') )
-    {
-        $cache = 'apc';
-        $session_handler = 'apc';
+    
+    public function __get($key)
+    {           
+        if ( isset($this->_key_map[$key]) )
+        {
+            $key = $this->_key_map[$key];
+        }
+        return isset($this->_data[$key]) ? $this->_data[$key] : null;
     }
-    $config = <<<EOF
-<?php
-class JConfig {
-
-	/* Site Settings */
-	var \$offline = '0';
-	var \$offline_message = 'This site is down for maintenance.<br /> Please check back again soon.';
-	var \$sitename = 'Anahita';			// Name of Anahita site
-	var \$editor = 'tinymce';
-	var \$list_limit = '20';
-	var \$legacy = '0';
-
-	/* Database Settings */
-	var \$dbtype = 'mysqli';					// Normally mysql
-	var \$host = '$db_host';				// This is normally set to localhost
-	var \$user = '$db_username';							// MySQL username
-	var \$password = '$db_password';						// MySQL password
-	var \$db = '$db_name';							// MySQL database name
-	var \$dbprefix = '$db_prefix';				// Do not change unless you need to!
-
-	/* Server Settings */
-	var \$secret = '$secret'; 		//Change this to something more secure
-	var \$gzip = '0';
-	var \$error_reporting = '0';
-	var \$helpurl = '';
-	var \$xmlrpc_server = '1';
-	var \$ftp_host = '';
-	var \$ftp_port = '';
-	var \$ftp_user = '';
-	var \$ftp_pass = '';
-	var \$ftp_root = '';
-	var \$ftp_enable = '';
-	var \$tmp_path	= '$target/tmp';
-	var \$log_path	= '$target/logs';
-	var \$offset = '0';
-	var \$live_site = ''; 					// Optional, Full url to Joomla install.
-	var \$force_ssl = 0;		//Force areas of the site to be SSL ONLY.  0 = None, 1 = Administrator, 2 = Both Site and Administrator
-
-	/* Session settings */
-	var \$lifetime = '15';					// Session time
-	var \$session_handler = '$session_handler';
-
-	/* Mail Settings */
-	var \$mailer = 'mail';
-	var \$mailfrom = '';
-	var \$fromname = '';
-	var \$sendmail = '/usr/sbin/sendmail';
-	var \$smtpauth = '0';
-	var \$smtpuser = '';
-	var \$smtppass = '';
-	var \$smtphost = 'localhost';
-
-	/* Cache Settings */
-	var \$caching = '1';
-	var \$cachetime = '15';
-	var \$cache_handler = '$cache';
-
-	/* Debug Settings */
-	var \$debug      = '0';
-	var \$debug_db 	= '0';
-	var \$debug_lang = '0';
-
-	/* Meta Settings */
-	var \$MetaDesc = 'Anahitaª is a remarkable open source platform and framework for developing various social networking services';
-	var \$MetaKeys = 'Anahita';
-	var \$MetaTitle = '1';
-	var \$MetaAuthor = '1';
-
-	/* SEO Settings */
-	var \$sef = '0';
-	var \$sef_rewrite = '0';
-	var \$sef_suffix = '';
-
-	/* Feed Settings */
-	var \$feed_limit   = 10;
-	var \$feed_email   = 'author';
-}
-?>
-EOF;
-    file_put_contents($target.'/configuration.php', $config);
+    
+    public function __set($key , $value)
+    {
+        if ( isset($this->_key_map[$key]) )
+        {
+            $key = $this->_key_map[$key];
+            $this->_data[$key] = $value;
+        }
+    }
+    
+    public function setDatabaseInfo($data)
+    {
+        $data['host'] = $data['host'].':'.$data['port'];
+        unset($data['port']);
+        $keys = array_map(function($key) {
+            return 'database_'.$key;
+        }, array_keys($data));
+        $data = array_combine($keys, array_values($data));
+        $this->set($data);
+    }
+    
+    public function getDatabaseInfo()
+    {                
+        $parts = explode(':', $this->database_host);
+        
+        return array(
+             'host'      => $parts[0],
+             'port'      => isset($parts[1]) ? $parts[1] : '3306', 
+             'user'      => $this->database_user,
+             'password'  => $this->database_password,
+             'name'      => $this->database_name,
+             'prefix'    => $this->database_prefix    
+        );
+    }
+    
+    public function toData()
+    {
+         return $this->_data;   
+    }
+    
+    public function save()
+    {
+        $data   = $this->toData();
+        $file   = new \SplFileObject($this->_configuration_file, 'w');
+        $file->fwrite("<?php\n");
+        $file->fwrite("class JConfig {\n\n");
+        $write = function($data) use($file) 
+        {
+            foreach($data as $key => $value)
+            {
+                if ( !is_numeric($value) ) {
+                    $value = "'".addslashes($value)."'";
+                }
+                $file->fwrite("   var \$$key = $value;\n");
+            }
+        };
+        $write_group = function($keys, $comment = null)
+             use ($data, $file, $write) 
+        {
+            if ( !empty($comment) ) {
+                $file->fwrite("   /*$comment*/\n");
+            }
+            $values = array();
+            foreach($keys as $key) {
+                $values[$key] = $data[$key];
+            }
+            $write($values);
+            $file->fwrite("\n");
+        };
+        $write_group(array('offline','offline_message','sitename','editor'), 'Site Settings');
+        $write_group(array('dbtype','host','user','password','db','dbprefix'), 'Database Settings');
+        $write_group(array('secret','error_reporting','tmp_path','log_path','force_ssl'), 'Server Settings');
+        $write_group(array('lifetime','session_handler'), 'Session Settings');
+        $write_group(array('mailer','mailfrom','fromname','sendmail','smtpauth','smtpuser','smtppass','smtphost'), 'Mail Settings');
+        $write_group(array('caching','cachetime','cache_handler'), 'Cache Settings');
+        $write_group(array('debug','debug_db','debug_lang'), 'Debug Settings');
+        $write_group(array('sef_rewrite'), 'Route Settings');
+        $file->fwrite("}");
+    }
 }
 ?>
