ed8cc2f3b1d36054152d4b9b93ddbc43fb805140
Arash  Sanieyan <ash@peerglobe.com>
2013-07-06 00:18:40 -0700

    updated

--- packages/Invites/src/media/com_invites/js/facebook.js
+++ packages/Invites/src/media/com_invites/js/facebook.js
@@ -16,7 +16,7 @@ var FacebookInvite = new Class({
 		}).get();
 	},
 	openDialog : function(token) {		
-		var msgLink = this.options.appURL.replace('www','') + '?token='+token.value;
+		var msgLink = this.options.appURL.replace('www.','') + '?token='+token.value;
 		console.log(msgLink);
 		FB.ui({
 				display: 'iframe',
