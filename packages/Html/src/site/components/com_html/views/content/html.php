90f95ab582bd6155eee8b296d3b97abcfcc3bbee
Arash  Sanieyan <ash@peerglobe.com>
2013-07-30 21:52:36 -0700

    if no basepath specified fallback on view default template paths

--- packages/Html/src/site/components/com_html/views/content/html.php
+++ packages/Html/src/site/components/com_html/views/content/html.php
@@ -25,7 +25,7 @@
  * @license    GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
  * @link       http://www.anahitapolis.com
  */
-class ComHtmlViewContentHtml extends LibBaseViewHtml
+class ComHtmlViewContentHtml extends ComBaseViewHtml
 {
     /**
      * Constructor.
