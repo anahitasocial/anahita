43537e6cd3cd4d21f535b468a10e177c5c32ee2e
Arash  Sanieyan <ash@peerglobe.com>
2013-08-02 19:28:36 -0700

    removed privacy_read_mode column

--- src/administrator/components/com_anahita/schemas/migrations/2.php
+++ src/administrator/components/com_anahita/schemas/migrations/2.php
@@ -23,7 +23,10 @@ class ComAnahitaSchemaMigration2 extends ComMigratorMigrationVersion
         dbexec('delete from jos_components where `option` IN ("com_installer","com_bazaar","com_tagmeta")');
         dbexec('delete from jos_components where `option` IS NULL OR `option` = ""');
         dbexec('update jos_components set admin_menu_link ="" where `option` IN ("com_search","com_todos","com_pages","com_html")');
-        //add your migration here
+        ///remove privacy_read_mode. some installation may still have it
+        try {
+            dbexec('alter table jos_anahita_nodes drop column `privacy_read_mode`');
+        } catch(Exception $e) {}
     }
 
    /**
