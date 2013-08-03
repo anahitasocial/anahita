70579106c71380952b15412c865eb5cd35fb6744
Arash  Sanieyan <ash@peerglobe.com>
2013-08-02 19:24:53 -0700

    fixed:missing semicolon

--- src/administrator/components/com_anahita/schemas/migrations/2.php
+++ src/administrator/components/com_anahita/schemas/migrations/2.php
@@ -20,7 +20,7 @@ class ComAnahitaSchemaMigration2 extends ComMigratorMigrationVersion
     */
     public function up()
     {
-        dbexec('delete from jos_components where `option` IN ("com_installer","com_bazaar","com_tagmeta")')
+        dbexec('delete from jos_components where `option` IN ("com_installer","com_bazaar","com_tagmeta")');
         dbexec('delete from jos_components where `option` IS NULL OR `option` = ""');
         dbexec('update jos_components set admin_menu_link ="" where `option` IN ("com_search","com_todos","com_pages","com_html")');
         //add your migration here
