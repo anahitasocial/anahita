5978cebccd55d3a4353cc663b1cfce9deeb1301e
Rastin Mehr <rastinmehr@Nautilus.local>
2013-08-05 16:33:45 -0700

    Note's meta title and description are added.

--- src/site/components/com_notes/views/note/html/default.php
+++ src/site/components/com_notes/views/note/html/default.php
@@ -1,5 +1,9 @@
 <?php defined('KOOWA') or die ?>
-<?php @title('Note') ?>
+
+<?php @title(sprintf(@text('COM-NOTES-META-TITLE-NOTE'), $actor->name).' - '.@date($note->creationTime)) ?>
+<?php @description(@helper('text.truncate', strip_tags($note->body), array('length'=>156))) ?>
+
+
 <?php @commands('toolbar') ?>
 
 <module position="sidebar-b" style="none"></module>
