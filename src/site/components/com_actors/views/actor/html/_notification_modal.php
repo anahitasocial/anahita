43e356394fe989989c9e71f97ee7a6941db4d3f0
Rastin Mehr <rastinmehr@Nautilus.local>
2013-08-07 17:32:33 -0700

    Changed translation labels from COM-NOTIFICATIONS to COM-ACTORS

--- src/site/components/com_actors/views/actor/html/_notification_modal.php
+++ src/site/components/com_actors/views/actor/html/_notification_modal.php
@@ -4,16 +4,16 @@
   
   <div class="modal-header">
     <a href="#" class="close">x</a>
-    <h3><?= @text('COM-NOTIFICATIONS-EDIT-NOTIFICATION-SETTINGS') ?></h3>
+    <h3><?= @text('COM-ACTORS-EDIT-NOTIFICATION-SETTINGS') ?></h3>
   </div>
   
   <div class="modal-body">
         <?php if ( $item->authorize('subscribe') ) : ?>
 		<form action="<?=@route($item->getURL())?>" method="post">
-        	<label class="control-label"><?= @text('COM-NOTIFICATIONS-RECIEVE-NOTIFICATIONS')?></label>                   
+        	<label class="control-label"><?= @text('COM-ACTORS-RECIEVE-NOTIFICATIONS')?></label>                   
             <input type="hidden" name="action" value="togglesubscription" />
             <select onchange="this.form.ajaxRequest().post()" >
-            	<?= @html('options', array(@text('COM-NOTIFICATIONS-RECIEVE-NOTIFICATIONS-NEW-SB'),@text('COM-NOTIFICATIONS-RECIEVE-NOTIFICATIONS-ONLY-SB')),$item->subscribed($viewer) ? 0 : 1) ?>
+            	<?= @html('options', array(@text('COM-ACTORS-RECIEVE-NOTIFICATIONS-NEW-SB'),@text('COM-ACTORS-RECIEVE-NOTIFICATIONS-ONLY-SB')),$item->subscribed($viewer) ? 0 : 1) ?>
              </select>
         </form>  
         <?php endif; ?>
@@ -24,7 +24,7 @@
             ))->reset();      
         ?>
         <form action="<?= @route('option=com_notifications&view=setting&oid='.$item->id)?>" method="post">                      
-			<label class="control-label"><?= @text('COM-NOTIFICATIONS-SEND-EMAIL')?></label>
+			<label class="control-label"><?= @text('COM-ACTORS-SEND-EMAIL')?></label>
 			<label class="checkbox">
 				<input onclick="this.form.ajaxRequest().post()" id="notification-email" name="email" <?= $setting->getValue('posts', 1) == 2 ? 'disabled="true"' : ''?> <?= $setting->sendEmail('posts', 1) && $setting->getValue('posts', 1) < 2 ? 'checked' : ''?> value="1" type="checkbox" />
 				<?= $viewer->email?><a href="<?=@route($viewer->getURL().'&get=settings&edit=account')?>" class="btn-mini"><?= @text('COM-ACTORS-NOTIFICATION-CHANGE-EMAIL') ?></a>
