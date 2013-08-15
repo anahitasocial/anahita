0d89e81ef347a43af158cad8d683411b23211f97
Rastin Mehr <rastinmehr@Nautilus.local>
2013-08-14 17:23:09 -0700

    Search result list item style has been updated

--- src/site/components/com_search/views/search/html/list.php
+++ src/site/components/com_search/views/search/html/list.php
@@ -3,6 +3,45 @@
 <?php if($item->isDescribable()): ?>
 <div class="an-entity">
 
+	<div class="clearfix">
+		<div class="entity-portrait-square">
+			<?php if($item->inherits('ComActorsDomainEntityActor')): ?>
+			<?= @avatar($item) ?>
+			<?php elseif($item->isModifiable()): ?>
+			<?= @avatar($item->author) ?>
+			<?php endif; ?>
+		</div>
+		
+		<div class="entity-container">
+			<?php if($item->inherits('ComMediumDomainEntityMedium')): ?>
+			<h4 class="author-name">
+				<?= @name($item->author) ?>
+			</h4>
+			<?php elseif($item->inherits('ComActorsDomainEntityActor')): ?>
+			<h4 class="entity-name">
+				<?= @name($item) ?>
+			</h4>
+			<?php endif; ?>
+			
+			<ul class="an-meta inline">
+				<?php if($item->inherits('ComMediumDomainEntityMedium')): ?>
+				<li><?= @date($item->creationTime) ?></li>
+				<?php endif; ?>
+				
+				<?php if($item->inherits('ComActorsDomainEntityActor')): ?>
+				<li>
+					<?= $item->followerCount ?>
+					<span class="stat-name"><?= @text('COM-ACTORS-SOCIALGRAPH-FOLLOWERS') ?></span> 
+					<?php if($item->isLeadable()): ?>
+					/ <?= $item->leaderCount ?>
+					<span class="stat-name"><?= @text('COM-ACTORS-SOCIALGRAPH-LEADERS') ?></span>
+					<?php endif; ?>
+				</li>
+				<?php endif; ?>
+			</ul>
+		</div>
+	</div>
+	
 	<?php if($item->isPortraitable() && !$item->inherits('ComActorsDomainEntityActor')): ?>
 	<div class="entity-portrait-medium">
 		<a title="<?= @escape($item->title) ?>" href="<?= @route($item->getURL()) ?>">			
@@ -10,62 +49,34 @@
 		</a>
 	</div>
 	<?php endif; ?>
-
-	<div class="entity-portrait-square">
-		<?php if($item->inherits('ComActorsDomainEntityActor')): ?>
-		<?= @avatar($item) ?>
-		<?php elseif($item->isModifiable()): ?>
-		<?= @avatar($item->author) ?>
-		<?php endif; ?>
+	
+	<?php if(!empty($item->title) && $item->inherits('ComMediumDomainEntityMedium')): ?>
+	<h3 class="entity-title">
+		<a href="<?= @route($item->getURL()) ?>">
+			<?= @escape($item->title) ?>
+		</a>
+	</h3>
+	<?php endif; ?>
+	
+	<div class="entity-description">
+		<?php $text = @helper('text.highlight', strip_tags($item->body), $keywords) ?>
+		<?= @helper('text.truncate', $text, array('length'=>400, 'consider_html'=>true))?>
 	</div>
 	
-	<div class="entity-container">
-		<?php if(!empty($item->title)): ?>
-		<h3 class="entity-title">
-			<a href="<?= @route($item->getURL()) ?>">
-				<?= @escape($item->title) ?>
-			</a>
-		</h3>
-		<?php endif; ?>
-		
-		<div class="entity-description">
-			<?php $text = @helper('text.highlight', strip_tags($item->body), $keywords) ?>
-			<?= @helper('text.truncate', $text, array('length'=>400, 'consider_html'=>true))?>
-		</div>
-		
-		<?php if($item->isModifiable()): ?>
-		<div class="entity-meta">
-			<?php if($item->inherits('ComActorsDomainEntityActor')): ?>
-			<div class="an-meta">
-				<?= $item->followerCount ?>
-				<span class="stat-name"><?= @text('COM-ACTORS-SOCIALGRAPH-FOLLOWERS') ?></span> 
-				
-				<?php if($item->isLeadable()): ?>
-				/ <?= $item->leaderCount ?>
-				<span class="stat-name"><?= @text('COM-ACTORS-SOCIALGRAPH-LEADERS') ?></span>
-				<?php endif; ?>
-			</div>
-			<?php endif; ?>
-			
-			<?php if($item->inherits('ComMediumDomainEntityMedium')): ?>
-			<div class="an-meta">
-				<?= sprintf(@text('LIB-AN-MEDIUM-AUTHOR'), @date($item->creationTime), @name($item->author)) ?>  
-			</div>
-			
-			<div class="an-meta">
-				<div class="vote-count-wrapper" id="vote-count-wrapper-<?= $item->id ?>">
-					<?= @helper('ui.voters', $item); ?>
-				</div>
-			</div>
-			
-			<div class="an-meta">
+	<?php if($item->inherits('ComMediumDomainEntityMedium')): ?>
+	<div class="entity-meta">
+		<ul class="an-meta inline">
+			<li>
 				<a href="<?= @route($item->getURL()) ?>">
 					<?= sprintf( @text('LIB-AN-MEDIUM-NUMBER-OF-COMMENTS'), $item->numOfComments) ?>
 				</a>
-			</div>
-			<?php endif; ?>
+			</li>
+		</ul>
+		
+		<div class="vote-count-wrapper an-meta" id="vote-count-wrapper-<?= $item->id ?>">
+			<?= @helper('ui.voters', $item); ?>
 		</div>
-		<?php endif; ?>
 	</div>
+	<?php endif; ?>
 </div>
 <?php endif; ?>
