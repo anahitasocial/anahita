9f1ac255aef9295ca2d2f13b47b0d139d2afa480
Arash  Sanieyan <ash@peerglobe.com>
2013-08-14 23:10:26 -0700

    reverted the scroll loader.

--- src/media/lib_anahita/js/libs/ScrollLoader.js
+++ src/media/lib_anahita/js/libs/ScrollLoader.js
@@ -29,14 +29,17 @@ var ScrollLoader = new Class({
     	var orientation = ( this.options.mode == 'vertical' ) ? 'y' : 'x';
     	var scroll 		= this.scrollable.getScroll()[orientation];
     	var scrollSize	= this.scrollable.getScrollSize()[orientation];
-    	var scrollWin   = this.scrollable.getSize()[orientation];
-    	
-    	//console.log('scroll size: ' + scrollSize);
-    	//console.log('fire: ' + Math.max(scrollSize - scrollWin * 2, 0));
-    	//console.log('scroll: ' + scroll);
-    	//console.log('---');
-    	
-    	if( (this.options.fixedheight && scroll < scrollSize) || (scroll > scrollSize - scrollWin * 0.6) )
+    	var scrollWin   = this.scrollable.getSize()[orientation];    	
+//    	console.log('scroll size: ' + scrollSize);
+//    	console.log('fire :' + Math.floor(scrollSize * 0.6));
+//    	console.log('fire: ' + Math.max(scrollSize - scrollWin * 2, 0));
+//    	console.log('scroll: ' + scroll);
+//    	console.log('---');    	    	
+    	if( (this.options.fixedheight && scroll < scrollSize)
+    				|| scroll > Math.floor(scrollSize * 0.6) ) { 
+    		console.log(this.scrollable.getScroll(),this.scrollable.getScrollSize());
     		this.fireEvent('scroll');
+    	}
+    		
     }
 });
