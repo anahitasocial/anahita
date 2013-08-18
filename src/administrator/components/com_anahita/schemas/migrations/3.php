284586f830aa55dc6a817ee03dcda692d27ae4b4
Arash  Sanieyan <ash@peerglobe.com>
2013-08-17 18:15:03 -0700

    adds the com_html to the back-end menu

--- src/administrator/components/com_anahita/schemas/migrations/3.php
+++ src/administrator/components/com_anahita/schemas/migrations/3.php
@@ -24,6 +24,7 @@ class ComAnahitaSchemaMigration3 extends ComMigratorMigrationVersion
         if ( !dbexists('SELECT id FROM #__components WHERE `option` = "com_html"') ) {
             $this[] = "INSERT INTO `#__components` VALUES(NULL, 'Html', 'option=com_html', 0, 0, 'option=com_html', 'Html', 'com_html', 0, 'js/ThemeOffice/component.png', 1, '', 1)";
         }
+        $this[] = "UPDATE #__components SET admin_menu_link='option=com_html', admin_menu_alt='Html', admin_menu_img='js/ThemeOffice/component.png' WHERE `option` = 'com_html'";        
         $this[] = "DELETE FROM #__plugins WHERE folder IN ('content','editors','editors-xtd') OR element = 'mtupgrade'";        
     }
 
