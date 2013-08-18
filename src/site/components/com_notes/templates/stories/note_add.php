cf60c568f2ab69a53784fac23e331f38e2784031
Arash  Sanieyan <ash@peerglobe.com>
2013-08-17 17:18:51 -0700

    simplified note_add

--- src/site/components/com_notes/templates/stories/note_add.php
+++ src/site/components/com_notes/templates/stories/note_add.php
@@ -1,23 +1,16 @@
 <?php defined('KOOWA') or die('Restricted access');?>
 
-<data name="title">
-<?php if ( $type != 'notification') : ?>	
-	<?php if( $object->access != 'public' && ($target->id == $subject->id || $target->eql($actor)) ): ?>
-    <i class="icon-lock"></i>  
-    <?php endif; ?>
+<data name="title">        	
+    <?php if ( $object->access != 'public' ) : ?>
+        <i class="icon-lock"></i>
+    <?php endif;?>		
     <?= sprintf(@text('COM-NOTES-STORY-ADD'), @name($subject), @route($object->getURL())) ?>
-<?php else: ?>
-    <?php if ( $type == 'notification' ) : ?>	
-	<?= sprintf(@text('COM-NOTES-STORY-ADD'), @name($subject), @route($object->getURL())) ?>    
-    <?php endif; ?>
-<?php endif; ?>
 </data>
 
-<?php if ( $type == 'story') : ?>
 <data name="body">    
 	<?= @helper('text.truncate', @content($object->body), array('length'=>200, 'consider_html'=>true, 'read_more'=>true)); ?>
 </data>
-<?php else : ?>
+<?php if ($type == 'notification') :?>
 <data name="email_body">
 <div><?= $object->body ?></div>
 <?php $commands->insert('viewstory', array('label'=>@text('COM-NOTES-VIEW-POST')))->href($object->getURL())?>
