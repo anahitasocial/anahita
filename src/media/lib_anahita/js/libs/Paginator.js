95103c162b87cbd933e4dc81dae3940a76a588ee
Pavel Kulbakin <p.kulbakin@gmail.com>
2013-08-24 01:19:17 +0300

    fix: javascript Paginator
    Paginator had batchSize option which supposedly load pages in batches,
    but it didn't and generally broken. It was impossible to set it to 0,
    i.e. disable batching, setting it to 1 would load 2 pages at once
    instead of 1, and it only gave effect for 1st load, subsequent requests
    for next page would load pages 1 by 1. So the only effect it gave is
    that after user opened an url, Paginator would load 3 (!) next pages in
    background.

--- src/media/lib_anahita/js/libs/Paginator.js
+++ src/media/lib_anahita/js/libs/Paginator.js
@@ -1,9 +1,9 @@
 var Paginator = new Class({
-    	
-    	Implements : [Options, Events],
-    	
-    	options : {
-	    	// onPageReady   : fn,
+    
+	Implements : [Options, Events],
+	
+	options : {
+    	// onPageReady   : fn,
     	resultHandler : null
     },
 	
@@ -12,8 +12,7 @@ var Paginator = new Class({
 	 * 
 	 * Hash options {}
 	 */
-	initialize : function(options) 
-	{
+	initialize : function(options) {
 		this.setOptions(options);
 		this.spinner   = options.spinner;    		
 		this.pages     = new Paginator.Pages(options.url, options);
@@ -24,13 +23,14 @@ var Paginator = new Class({
 	/**
 	 * Shows the next page
 	 */
-	showNextPage : function() 
-	{
-		this.pages.get(this.nextPage, function(page) {    			
-			//console.log('handling results for page ' + page.number)
-			this.fireEvent('pageReady',[page]);
-    		this.nextPage++;
-		}.bind(this));
+	showNextPage : function() {
+		if ( ! this.pages.stopPagination) {
+			this.pages.get(this.nextPage, function(page) {    			
+				//console.log('handling results for page ' + page.number)
+				this.fireEvent('pageReady',[page]);
+	    		this.nextPage++;
+			}.bind(this));
+		}
 	}
 });
 
@@ -48,9 +48,7 @@ Paginator.Pages = new Class({
      */
     options : {
     	limit	 	    : 20,
-    	resultSelector  : null,
-    	startImmediatly : true,
-    	batchSize	    : 2
+    	resultSelector  : null
     },
     
     /**
@@ -59,37 +57,28 @@ Paginator.Pages = new Class({
      * String  url   pages base url
      * int     limit limit per page
      */
-    initialize : function(url, options) 
-    {
+    initialize : function(url, options) {
     	//console.log('create pages for base url ' + url);
     	this.url 	 = new URI(url);
-    	this.setOptions(options);        	
+    	this.setOptions(options);
     	this.requests = new Request.Queue({
-    		concurrent : this.options.batchSize
+    		concurrent : 1
     	});
     	this.limit    = this.options.limit;
     	this.resultSelector  = this.options.resultSelector;
-    	this.currentBatch = 0;
-    	if ( this.options.startImmediatly )
-    		this._getBatch();
     },
     
-    get  : function(number, onsuccess) 
-    {
+    get  : function(number, onsuccess) {
     	var page = this._getPage(number);
     	
-    	if ( onsuccess ) 
-    	{
+    	if ( onsuccess ) {
     		//if the request is still running then add a success event
-    		if ( page.request.isRunning() ) 
-    		{
-    			if ( !page.request.registered ) {
+    		if ( page.request.isRunning() )  {
+    			if ( ! page.request.registered ) {
     				page.request.addEvent('success', onsuccess.bind(null,[this.pages[number]]));
     				page.request.registered = true;
     			}
-        	}
-    		else 
-    		{
+        	} else {
     			//if the request has finished running and hasn't been registered
     			//then call on onsuccess
     			if ( !page.request.registered ) {
@@ -102,52 +91,34 @@ Paginator.Pages = new Class({
     },
     
     /**
-     * Gets a next batch
-     */
-    _getBatch : function()
-    {
-    	var start = (this.options.batchSize * this.currentBatch) + 1;
-    	var end = start + this.options.batchSize;
-    	//console.log('getting a batch ' + start + ' to ' + end, this.options.batchSize, this.currentBatch);
-    	//always create a batch of pages
-    	for(i=start;i<=end;i++) {
-    		this._getPage(i);
-    	}
-    },
-    
-    /**
      * Creates a page using a number. 
      *  
      */
-    _getPage : function(number)
-    {
-    	//if a page doesn't exists then queue batchSize of pages
-    	if ( !this.pages[number] ) 
-    	{        		
-    		
+    _getPage : function(number) {
+    	if ( ! this.pages[number] ) {
     		var self  = this;
     		var page  = {
-        		number   : number,
-        		entities : null,
-        		request  : new Request({
-            		url 	: Object.clone(this.url).setData({start:number * this.limit, limit:this.limit}, true).toString(),
-            		method  : 'get',
-            		onSuccess : function() {
-            			self.pages[number].entities = this.response.text.parseHTML().getElements(self.resultSelector);
-            			if ( self.pages[number].entities.length < self.limit ) {
-            				self.stopPagination = true;
-            			}
-            	//		console.log('fetched page ' + number + ' with ' + self.pages[number].entities.length + ' entities');
-            		}
-            	})
-        	};
+				number   : number,
+				entities : null,
+				request  : new Request({
+					url 	: Object.clone(this.url).setData({start:number * this.limit, limit:this.limit}, true).toString(),
+					method  : 'get',
+					onSuccess : function() {
+    					self.pages[number].entities = this.response.text.parseHTML().getElements(self.resultSelector);
+    					if ( self.pages[number].entities.length < self.limit ) {
+    						self.stopPagination = true;
+    					}
+    					//console.log('fetched page ' + number + ' with ' + self.pages[number].entities.length + ' entities');
+    				}
+				})
+    		};
     		this.pages[number] = page;
     		//console.log('fetching page ' + number );
-    		if ( !this.stopPagination ) {
+    		if ( ! this.stopPagination ) {
     			this.requests.addRequest(number, page.request).send(number);
     		}
     	}
     	return this.pages[number];
     }
-    
+
 });
