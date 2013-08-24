e4cf24cc83d17ba8234326cd2f7273bb3a71d907
Rastin Mehr <rastin@anahitapolis.com>
2013-08-24 00:38:45 -0700

    added a new default homepage

--- src/site/templates/shiraz/html/com_html/content/default.php
+++ src/site/templates/shiraz/html/com_html/content/default.php
@@ -1,6 +1,45 @@
 <div class="heredoc">
-<?php @title('Page Title')?>
+<?php @title('Anahita Social Networking Platform and Framework')?>
 <?php @description('Page Description')?>
-<h1>Welcome to Anahita</h1>
-<a href="<?= @route('layout=about') ?>">About us</a>
+
+<div class="row">
+	<div class="span8 offset2">
+		<div class="hero-unit text-center">
+			<img src="https://s3.amazonaws.com/anahitapolis.com/media/logos/homepage_logo.png" alt="Anahita social networking platform and framework" />
+			
+			<h1>Anahita</h1>
+			
+			<p class="lead">A remarkable social networking platform and framework for connecting people, places, and things.</p>
+			
+			<p>
+				<a class="btn btn-primary btn-large" href="http://GetAnahita.com" target="_blank">
+					GetAnahita.com
+				</a>
+				
+				<a title="Twitter" class="btn btn-twitter  btn-large" href="https://www.twitter.com/anahitapolis" target="_blank">
+					<i class="icon-twitter"></i>
+				</a> 
+				
+				<a title="Facebook" class="btn btn-facebook  btn-large" href="https://www.facebook.com/anahitasocial" target="_blank">
+					<i class="icon-facebook"></i>
+				</a> 
+				
+				<a title="Google+" class="btn btn-googleplus  btn-large" href="https://plus.google.com/111222489368350494929/posts" target="_blank">
+					<i class="icon-googleplus"></i>
+				</a>
+				
+				<a title="Blog" class="btn btn-feed  btn-large" href="http://blog.anahitapolis.com" target="_blank">
+					<i class="icon-feed"></i>
+				</a> 
+				
+				<a title="GitHub" class="btn btn-github btn-large" href="https://www.github.com/anahitasocial" target="_blank">
+					<i class="icon-github"></i>
+				</a>
+			</p>
+			
+			<p>Follow <strong>#Anahita</strong></p>
+		</div>
+	</div>
 </div>
+
+
