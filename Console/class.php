819129bc19747ec8d559be70c90b97a349f00df2
Arash  Sanieyan <ash@peerglobe.com>
2013-08-26 15:39:50 -0700

    unlinking dead links does not use os command

--- Console/class.php
+++ Console/class.php
@@ -51,12 +51,12 @@ class Mapper
         foreach($this->_maps as $map) {
             $map->unlink();
         }
-        $deadlinks = explode("\n", trim(`find -L {$this->_target_root} -type l -lname '*'`));
-        $deadlinks = array_filter($deadlinks);
-        if ( count($deadlinks) )
+        $files     = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($this->_target_root));
+        //deleting deadlinks
+        foreach($files as $file) 
         {
-            foreach($deadlinks as $link) {         
-                @unlink($link);
+            if ( is_link($file) && !file_exists(realpath($file))) {
+                unlink($file);
             }
         }
     }
