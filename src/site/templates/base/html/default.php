65cc467e870ea05b0652e308ea9855119f33d78f
Rastin Mehr <rastin@anahitapolis.com>
2013-12-14 19:42:04 -0800

    removed lan tag

--- src/site/templates/base/html/default.php
+++ src/site/templates/base/html/default.php
@@ -1,6 +1,6 @@
 <?php defined('KOOWA') or die;?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
   <head>
   	<meta name="viewport" content="width=device-width, initial-scale=1.0">
   	<?= @render('favicon') ?>
