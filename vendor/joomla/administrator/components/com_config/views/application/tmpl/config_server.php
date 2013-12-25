51ee7728211d9b375be66dc4f44a4533e7b1ada4
Rastin Mehr <rastin@anahitapolis.com>
2013-12-25 15:51:25 -0800

    joomla legacy parameters removed from the UI

--- vendor/joomla/administrator/components/com_config/views/application/tmpl/config_server.php
+++ vendor/joomla/administrator/components/com_config/views/application/tmpl/config_server.php
@@ -17,32 +17,12 @@ defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
 			</tr>
 			<tr>
 				<td class="key">
-					<span class="editlinktip hasTip" title="<?php echo JText::_( 'GZIP Page Compression' ); ?>::<?php echo JText::_( 'Compress buffered output if supported' ); ?>">
-						<?php echo JText::_( 'GZIP Page Compression' ); ?>
-					</span>
-				</td>
-				<td>
-					<?php echo $lists['gzip']; ?>
-				</td>
-			</tr>
-			<tr>
-				<td class="key">
 					<span class="editlinktip hasTip" title="<?php echo JText::_( 'Error Reporting' ); ?>::<?php echo JText::_( 'TIPERRORREPORTING' ); ?>">
 						<?php echo JText::_( 'Error Reporting' ); ?>
 					</span>
 				</td>
 				<td>
 					<?php echo $lists['error_reporting']; ?>
-				</td>
-			</tr>
-			<tr>
-				<td class="key">
-					<span class="editlinktip hasTip" title="<?php echo JText::_('Force SSL'); ?>::<?php echo JText::_( 'TIPFORCESSL' ); ?>">
-						<?php echo JText::_('Force SSL'); ?>
-					</span>
-				</td>
-				<td>
-					<?php echo $lists['force_ssl']; ?>
 				</td>
 			</tr>
 		</tbody>
