b8b96ce1c44fd3408098416f70d388b34d2493ec
Rastin Mehr <rastin@anahitapolis.com>
2014-02-22 21:05:37 -0800

    A list of strict standard issues are resolved. Had to modify the ordering methods to become more consistent with the Nooku method calls. The com_components list view is also improved.

--- packages/Subscriptions/src/administrator/components/com_subscriptions/views/packages/html/default_item.php
+++ packages/Subscriptions/src/administrator/components/com_subscriptions/views/packages/html/default_item.php
@@ -1,22 +1,20 @@
 <? defined('KOOWA') or die('Restricted access'); ?>
 
-<? $i = 0; $m = 0; ?>
-<? foreach ($packages as $package) : ?>
+<?php $i = 0; $m = 0; ?>
+<?php foreach ($packages as $package) : ?>
 <tr class="<?php echo 'row'.$m; ?>">
 	<td align="center"><?= $i + 1; ?></td>
 	<td align="center"><?= @helper('grid.checkbox', array('row'=>$package)); ?></td>
 	<td>
-		<span class="editlinktip hasTip" title="<?= @escape($package->getName()); ?>">
-			<a href="<?= @route('view=package&id='.$package->id)?>">
-				<?= @escape($package->getName()); ?>				
-			</a>
-		</span>
+		<a href="<?= @route('view=package&id='.$package->id)?>">
+			<?= @escape($package->getName()); ?>				
+		</a>
 	</td>
 	<td align="center">
-		<?= @helper('grid.enable', $package, array('url'=>@route('view=package&id='.$package->id))); ?>
+		<?= @helper('grid.enable', array('row'=>$package, 'url'=>@route('view=package&id='.$package->id))); ?>
 	</td>
 	<td align="center">
-		<?= @helper('grid.order',  $package, array('url'=>@route('view=package&id='.$package->id))); ?>
+		<?= @helper('grid.order', array('row'=>$package, 'url'=>@route('view=package&id='.$package->id))); ?>
 	</td>
 	<td align="center"><?=$package->subscriptions->getTotal()?></td>
 	<td align="center"><?= $package->price ?> <?= get_config_value('subscriptions.currency','US') ?></td>
