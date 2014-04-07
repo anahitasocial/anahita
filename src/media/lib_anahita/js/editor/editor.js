61a9f3eb3862368545edad619b70cb6196306787
Rastin Mehr <rastin@anahitapolis.com>
2014-04-06 19:17:58 -0700

    Fixed the issue with excessive br tags in the visual editor. It is a hack until we replace the editor with something better. Such as Markdown.

--- src/media/lib_anahita/js/editor/editor.js
+++ src/media/lib_anahita/js/editor/editor.js
@@ -193,19 +193,24 @@ var AnEditor = new Class({
 	},
 	_cleanupEditor : function(type, content)
 	{		 		  		 
-		  switch (type) 
-		  {
-		   case "get_from_editor":
-			 content  = content.replace(/<br>/gi,"\n").replace(/<br\s*\/>/gi, "\n");			 
-			 content  = this._formatText(content, type);
-			 content  = Encoder.htmlDecode(content);
-		     break;
-		   case "insert_to_editor":					 
-			 content  = this._formatText(content, type);			
-			 content  = content.replace(/\n/gi,'<br />');
-		     break;
-		  }
-		 return content;
+		switch (type) 
+		{
+		  	case "get_from_editor":
+		  		content  = content
+		  					.replace(/<br>\n*<br>/gmi,"\n\n")
+		  					.replace(/<br\s*\/>\n*<br\s*\/>/gmi, "\n\n")
+		  					.replace(/<br\s*\/>/gmi, "");
+		  			
+		  		content  = this._formatText(content, type);
+		  		content  = Encoder.htmlDecode(content);
+		    break;
+		     
+		  	case "insert_to_editor":					 
+		  		content  = this._formatText(content, type);			
+		  		content  = content.replace(/\r?\n\r?\n/gm,'<br /><br />');
+		    break;
+		}
+		return content;
 	},
 	_formatText : function(content, type) 
 	{
