5a4192d840c30456317cb5cfdd3998c95dc46907
Rastin Mehr <rastin@anahitapolis.com>
2014-05-19 16:35:19 -0700

    The Anahita 4.0 migration script. So far it prepares the nodes table for hashtags and extracts the existing terms from the nodes and creates actual hashtag records.

--- src/administrator/components/com_anahita/schemas/migrations/5.php
+++ src/administrator/components/com_anahita/schemas/migrations/5.php
@@ -20,10 +20,54 @@ class ComAnahitaSchemaMigration5 extends ComMigratorMigrationVersion
     */
     public function up()
     {
-        dbexec('ALTER TABLE #__anahita_nodes DROP COLUMN `tag_count`');
+        
+    	//create the fields required for creating hashtag nodes
+    	dbexec('ALTER TABLE #__anahita_nodes DROP COLUMN `tag_count`');
         dbexec('ALTER TABLE #__anahita_nodes CHANGE `tag_ids` `hashtag_ids` TEXT DEFAULT NULL');
         dbexec('ALTER TABLE #__anahita_nodes ADD `hashtagable_count` INT(11) UNSIGNED DEFAULT NULL AFTER `hashtag_ids`');
         dbexec('ALTER TABLE #__anahita_nodes ADD `hashtagable_ids` TEXT DEFAULT NULL AFTER `hashtagable_count`');
+    	
+        $timeThen = microtime(true);
+        $ids = array();
+        
+    	//extracting hashtag terms from actors
+        $ids = dbfetch('SELECT id FROM #__anahita_nodes WHERE type LIKE \'ComActorsDomainEntityActor%\'');
+
+    	foreach($ids as $id)
+    	{
+    		$entity = KService::get('com://site/actors.domain.entity.actor')->getRepository()->getQuery()->disableChain()->fetch($id);    		
+    		$hashtagTerms = $this->extractHashtagTerms($entity->description);
+    		
+    		foreach($hashtagTerms as $term)
+    			$entity->addHashtag(trim($term))->save();
+    	}
+        
+    	//extracting hashtag terms from comments
+    	$ids = dbfetch('SELECT id FROM #__anahita_nodes WHERE type LIKE \'ComBaseDomainEntityComment%\'');
+    	
+    	foreach($ids as $id)
+    	{
+    		$entity = KService::get('com://site/base.domain.entity.comment')->getRepository()->getQuery()->disableChain()->fetch($id);    		
+    		$hashtagTerms = $this->extractHashtagTerms($entity->body);
+    		
+    		foreach($hashtagTerms as $term)
+    			$entity->addHashtag(trim($term))->save();
+    	}
+    	
+    	//extracting hashtag terms from mediums
+		$ids = dbfetch('SELECT id FROM #__anahita_nodes WHERE type LIKE \'ComMediumDomainEntityMedium%\' AND type NOT LIKE \'%com:topics.domain.entity.board\' ');
+    	
+    	foreach($ids as $id)
+    	{
+    		$entity = KService::get('com://site/medium.domain.entity.medium')->getRepository()->getQuery()->disableChain()->fetch($id);    		
+    		$hashtagTerms = $this->extractHashtagTerms($entity->description);
+    		
+    		foreach($hashtagTerms as $term)
+    			$entity->addHashtag(trim($term))->save();
+    	}
+    	
+    	$timeDiff = microtime(true) - $timeThen;
+        dboutput("TIME: ($timeDiff)"."\n");
     }
 
    /**
@@ -33,4 +77,19 @@ class ComAnahitaSchemaMigration5 extends ComMigratorMigrationVersion
     {
         //add your migration here        
     }
+    
+	/**
+	 * extracts a list of hashtag terms from a given text
+	 * 
+	 * @return array
+	 */
+	private function extractHashtagTerms($text)
+	{
+        $matches = array();
+        
+        if(preg_match_all(ComHashtagsDomainEntityHashtag::PATTERN_HASHTAG, $text, $matches))
+        	return array_unique($matches[1]);
+        else
+        	return array();
+	} 
 }
