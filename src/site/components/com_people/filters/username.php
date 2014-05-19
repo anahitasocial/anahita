95d5462413a9c45bbb1a8e401c05df8620905ef9
Rastin Mehr <rastin@anahitapolis.com>
2014-05-19 16:38:55 -0700

    class now extends KFilterAbstract instead of KFilterCmd also added implementation of the sanitize method.

--- src/site/components/com_people/filters/username.php
+++ src/site/components/com_people/filters/username.php
@@ -26,7 +26,7 @@
  * @license    GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
  * @link       http://www.anahitapolis.com
  */
-class ComPeopleFilterUsername extends KFilterCmd
+class ComPeopleFilterUsername extends KFilterAbstract
 {
 	/**
 	 * Validate a value
@@ -49,7 +49,8 @@ class ComPeopleFilterUsername extends KFilterCmd
      */
     protected function _sanitize($value)
     {
-        //don't allow to sanitize that way we can return an error
-        return $value;        
+        $value = trim($value);
+        $pattern = '/^[A-Za-z0-9][A-Za-z0-9_-]*$/';
+        return preg_replace($pattern, '', $value);
     }
 }
