92e6ce45cc7199ccd730b0eb88cb5b65925e7796
Rastin Mehr <rastin@anahitapolis.com>
2014-05-31 17:55:09 -0700

    hashtags layout added hashtags translation file added

---
+++ src/site/components/com_hashtags/views/hashtags/html/default.php
@@ -0,0 +1,9 @@
+<?php defined('KOOWA') or die; ?>
+
+<ul>
+<?php foreach($items as $item): ?>
+	<li>
+		<a href="<?= @route($item->getURL()) ?>"><?= $item->name ?></a>
+	</li>
+<?php endforeach; ?>
+</ul>
