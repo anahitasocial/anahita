4dc0f2f451402e64e803d1968ae39531bea9f5d6
Rastin Mehr <rastin@anahitapolis.com>
2014-06-14 19:48:30 -0700

    display hashtagable gadgets

--- src/site/components/com_hashtags/views/hashtag/html/default.php
+++ src/site/components/com_hashtags/views/hashtag/html/default.php
@@ -1,3 +1,32 @@
 <?php defined('KOOWA') or die; ?>
 
-#<?= $item->name ?> <?= $item->id ?>
+<module position="sidebar-b" style="none"></module>
+
+<?php if(count($gadgets) >= 1): ?>
+<module position="sidebar-a">   
+<ul class="nav nav-pills nav-stacked sidelinks" data-behavior="BS.Tabs" data-bs-tabs-options="{'smooth':true,'tabs-selector':'.profile-tab-selector a','sections-selector':'! * .profile-tab-content'}">   
+<?php $i=0; ?>
+<?php foreach($gadgets as $index=>$gadget) : ?>
+	<li class="profile-tab-selector <?= ($i == 0) ? 'active' : ''; ?>">
+		<a href="#"><?= $gadget->title ?></a>
+    </li>
+<?php $i++; ?>    
+<?php endforeach;?>
+</ul>
+</module>
+
+<?php endif; ?>
+
+<h1 id="entity-name"><?= sprintf(@text('COM-HASHTAG-TERM'), $item->name) ?></h1>
+
+<?php if(!empty($item->body)): ?>
+<div id="entity-description">
+	<?= @helper('text.truncate', @escape($item->body), array('length'=>250, 'read_more'=>true)); ?>
+</div>
+<?php endif; ?>
+
+<?php foreach($gadgets as $gadget) : ?>
+<div class="profile-tab-content">		
+	<?= @helper('ui.gadget', $gadget) ?>
+</div>		
+<?php endforeach;?>
