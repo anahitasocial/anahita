696d5a2cd36825ddd7d18979b81e5e4c534f76ff
Rastin Mehr <rastin@anahitapolis.com>
2014-06-14 19:51:52 -0700

    hashtag profile view class. This is a work in progress obviously.

---
+++ src/site/components/com_hashtags/views/hashtag/html.php
@@ -0,0 +1,53 @@
+<?php
+
+/** 
+ * LICENSE: ##LICENSE##
+ * 
+ * @category   Anahita
+ * @package    Com_Hashtags
+ * @subpackage View
+ * @author     Rastin Mehr <rastin@anahitapolis.com>
+ * @copyright  2008 - 2014 rmdStudio Inc.
+ * @license    GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
+ * @link       http://www.GetAnahita.com
+ */
+
+/**
+ * Default Actor View (Profile View)
+ *
+ * @category   Anahita
+ * @package    Com_Hashtags
+ * @subpackage View
+ * @author     Rastin Mehr <rastin@anahitapolis.com>
+ * @license    GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
+ * @link       http://www.GetAnahita.com
+ */
+class ComHashtagsViewHashtagHtml extends ComBaseViewHtml
+{
+	/**
+	 * Prepare default layout
+	 * 
+	 * @return void
+	 */
+	protected function _layoutDefault()
+	{	
+		$this->set('gadgets', new LibBaseTemplateObjectContainer());
+		$scopes = $this->getService('com://site/hashtags.domain.entityset.scope');
+
+		$context = new KCommandContext();
+		$context->gadgets = $this->gadgets;
+		
+		foreach($scopes as $scope)
+		{	
+			$scopeKey = explode('.', $scope->getKey());
+			$view = KInflector::pluralize($scopeKey[1]);
+			$option = 'com_'.$scopeKey[0];
+			
+			$context->gadgets->insert($scope->getKey(), array(             
+                'title' => translate('COM-'.str_ireplace('.', '-SEARCH-SCOPE-', strtoupper($scope->getKey()))),
+				'url' => 'option='.$option.'&view='.$view.'&layout=gadget&ht[]='.$this->item->alias,
+                'title_url'	=> 'option='.$option.'&view='.$view.'&ht[]='.$this->item->alias
+        	));
+		}
+	}
+}
