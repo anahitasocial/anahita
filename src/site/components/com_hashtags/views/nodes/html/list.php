5932e866c1fafae972dc61eed3335cb8cf0f2d9c
Rastin Mehr <rastin@anahitapolis.com>
2014-07-01 23:08:57 -0700

    added node list layouts for now. Perhaps we should move them to the base class later on.

---
+++ src/site/components/com_hashtags/views/nodes/html/list.php
@@ -0,0 +1,20 @@
+<?php defined('KOOWA') or die('Restricted access');?>	
+				
+<?php if(count($items)) :?>
+	<?php $view = @view('node'); ?>
+	<?php foreach($items as $item): ?>
+	
+	<?php if($item->inherits('ComBaseDomainEntityComment')): ?>
+	<?= $view->layout('list_comment')->item($item) ?>
+	<?php elseif($item->inherits('ComActorsDomainEntityActor')): ?>
+	<?= $view->layout('list_actor')->item($item) ?>
+	<?php elseif($item->inherits('ComMediumDomainEntityMedium')): ?>
+	<?= $view->layout('list_medium')->item($item) ?>
+	<?php else: ?>
+	<?= $view->layout('list')->item($item) ?>
+	<?php endif; ?>
+	
+	<?php endforeach; ?>
+<?php else: ?>
+<?= @message(@text('LIB-AN-NODES-EMPTY-LIST-MESSAGE')) ?>
+<?php endif; ?>
