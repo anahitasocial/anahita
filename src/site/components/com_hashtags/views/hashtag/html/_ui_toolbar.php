b80a38cdfd8fc7335c8b9ecddf14001bd71eb0a0
Rastin Mehr <rastin@anahitapolis.com>
2014-07-18 17:21:06 -0700

    newest or recent items is now the default

--- src/site/components/com_hashtags/views/hashtag/html/_ui_toolbar.php
+++ src/site/components/com_hashtags/views/hashtag/html/_ui_toolbar.php
@@ -1,14 +1,14 @@
 <?php defined('KOOWA') or die('Restricted access');?>
 
-<div class="btn-toolbar clearfix" data-behavior="BS.Dropdown">
+<div class="btn-toolbar clearfix">
     <div class="pull-right btn-group">
-        <a class="btn <?= ($sort == 'top') ? 'disabled' : '' ?>" href="<?= @route(array('view'=>'hashtag', 'alias'=>$item->name, 'sort'=>'top')) ?>">
-            <i class="icon-fire"></i>
-            <?= @text('COM-HASHTAGS-HASHTAGABLES-SORT-TOP') ?>
-        </a> 
         <a class="btn <?= ($sort != 'top') ? 'disabled' : '' ?>" href="<?= @route(array('view'=>'hashtag', 'alias'=>$item->name, 'sort'=>'recent')) ?>">
             <i class="icon-time"></i>
             <?= @text('COM-HASHTAGS-HASHTAGABLES-SORT-RECENT') ?>
         </a>
+        <a class="btn <?= ($sort == 'top') ? 'disabled' : '' ?>" href="<?= @route(array('view'=>'hashtag', 'alias'=>$item->name, 'sort'=>'top')) ?>">
+            <i class="icon-fire"></i>
+            <?= @text('COM-HASHTAGS-HASHTAGABLES-SORT-TOP') ?>
+        </a> 
     </div>
 </div>
