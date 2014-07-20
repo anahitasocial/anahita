afaf40c79466fc6a81337022b9df6fae148f7ac6
Rastin Mehr <rastin@anahitapolis.com>
2014-07-19 18:41:55 -0700

    jazzed up the hashtags default layout a bit

--- src/site/components/com_hashtags/views/hashtags/html/default.php
+++ src/site/components/com_hashtags/views/hashtags/html/default.php
@@ -1,9 +1,13 @@
 <?php defined('KOOWA') or die; ?>
 
-<ul>
+<?php if(count($items)): ?>
+<ul class="nav nav-pills">
 <?php foreach($items as $item): ?>
 	<li>
 		<a href="<?= @route($item->getURL()) ?>">#<?= $item->name ?></a>
 	</li>
 <?php endforeach; ?>
 </ul>
+<?php else: ?>
+<?= @message(@text('LIB-AN-NODES-EMPTY-LIST-MESSAGE')) ?>
+<?php endif; ?>
