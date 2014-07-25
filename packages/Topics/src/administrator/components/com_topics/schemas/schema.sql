70f61434cb3a52132bde1b40fa6683cbb9ba16c3
Rastin Mehr <rastin@anahitapolis.com>
2014-07-25 11:45:44 -0700

    don't create the boards table

--- packages/Topics/src/administrator/components/com_topics/schemas/schema.sql
+++ packages/Topics/src/administrator/components/com_topics/schemas/schema.sql
@@ -1,16 +1,5 @@
 -- --------------------------------------------------------
 
-CREATE TABLE `#__topics_boards` (
-  `topics_board_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `node_id` bigint(11) unsigned NOT NULL DEFAULT '0',
-  `topics_count` int(11) NOT NULL DEFAULT '0',
-  `params` text NOT NULL,
-  PRIMARY KEY (`topics_board_id`),
-  KEY `node_id` (`node_id`)
-) ENGINE=InnoDB;
-
--- --------------------------------------------------------
-
 CREATE TABLE `#__topics_topics` (
   `id` bigint(11) unsigned NOT NULL AUTO_INCREMENT,
   `node_id` bigint(11) unsigned NOT NULL DEFAULT '0',
