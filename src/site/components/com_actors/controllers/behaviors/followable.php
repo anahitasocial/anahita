0d6d4c9cedeacba7b56c4e1e267c37fce0080202
Rastin Mehr <rastin@anahitapolis.com>
2014-09-01 17:48:14 -0700

    changed my mind. add follower and add leasable are now using completely different methods.

--- src/site/components/com_actors/controllers/behaviors/followable.php
+++ src/site/components/com_actors/controllers/behaviors/followable.php
@@ -39,13 +39,12 @@ class ComActorsControllerBehaviorFollowable extends KControllerBehaviorAbstract
         parent::__construct($config);
         
         $config->mixer->registerCallback(
-            array('before.deletefollower','before.addfollower',
+            array('before.deletefollower','before.addfollower', 'before.addleadable',
                   'before.addrequester','before.deleterequester',
                   'before.addblocked','before.deleteblocked'), 
                   array($this, 'getActor'));
                   
         $config->mixer->registerActionAlias('follow', 'addfollower');
-        
         $config->mixer->registerActionAlias('unfollow', 'deletefollower');
                    
     }
@@ -97,32 +96,55 @@ class ComActorsControllerBehaviorFollowable extends KControllerBehaviorAbstract
 		if(!$this->getItem()->leading($this->actor))
 		{
 		    $this->getItem()->addFollower($this->actor);
-		   
-		    $story = $this->createStory(array(
-		    		'name' => 'actor_follow',
-		        	'subject' => $this->actor,
-		        	'owner' => $this->actor,
-		        	'target' => $this->getItem()
-		    	));
 		    
-		    if($this->viewer->eql($this->actor))
-		    {
-		    	$this->createNotification(array(
-		    		'name' => 'actor_follow',
-		    		'subject' => $this->actor, 
-		    		'target' => $this->getItem()
-		    	));
-		    }
-		    else 
-		    { 
-		    	$this->createNotification(array(
-		    		'name' => 'actor_leadable_add',
-		    		'subject' => $this->viewer,
-		    		'target' => $this->getItem(),
-		    		'object' => $this->actor,
-		    		'subscribers' => array($this->actor->id)
-		    	));
-		    }
+	    	$story = $this->createStory(array(
+	    		'name' => 'actor_follow',
+	        	'subject' => $this->actor,
+	        	'owner' => $this->actor,
+	        	'target' => $this->getItem()
+	    	));
+	    	
+	    	$this->createNotification(array(
+	    		'name' => 'actor_follow',
+	    		'subject' => $this->actor, 
+	    		'target' => $this->getItem()
+	    	));
+		}
+        
+        return $this->getItem();
+	}
+	
+	/**
+	 * Viewer adds a follower to the current actor resource. status is set to 
+     * KHttpResponse::RESET_CONTENT;
+	 * 
+	 * @param KCommandContext $context Context Parameter
+	 * 
+	 * @return void 
+	 */
+	protected function _actionAddleadable(KCommandContext $context)
+	{
+		$this->getResponse()->status = KHttpResponse::RESET_CONTENT;
+        
+		if(!$this->getItem()->leading($this->actor))
+		{
+		    $this->getItem()->addFollower($this->actor);
+		    
+	    	$this->createStory(array(
+	    		'name' => 'actor_leadable_add',
+	    		'owner' => $this->getItem(),
+	    		'subject' => $this->viewer,
+	    		'object' => $this->actor,
+	    		'target' => $this->getItem()
+	    	));
+	    	
+	    	$this->createNotification(array(
+	    		'name' => 'actor_leadable_add',
+	    		'subject' => $this->viewer,
+	    		'object' => $this->actor,
+	    		'target' => $this->getItem(),
+	    		'subscribers' => array($this->actor->id)
+	    	));
 		}
         
         return $this->getItem();
