02da70c540b3c13582facbed3c877b92a63ec459
Rastin Mehr <rastin@anahitapolis.com>
2014-09-07 02:08:10 -0700

    follower add makes more sense

---
+++ src/site/components/com_actors/templates/stories/actor_follower_add.php
@@ -0,0 +1,10 @@
+<?php defined('KOOWA') or die('Restricted access');?>
+
+<data name="title">
+<?php if ( $type != 'notification' ) : ?>
+<?=sprintf(@text('COM-ACTORS-STORY-FOLLOWER-ADD'), @name($subject), @name($object), @name($target)) ?>
+<?php else: ?>
+<?=sprintf(@text('COM-ACTORS-NOTIFICATION-FOLLOWER-ADD'), @name($subject), @name($object), @name($target)) ?>
+<?php endif; ?>
+</data>
+
