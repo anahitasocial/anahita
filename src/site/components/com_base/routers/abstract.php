5ded13e19694b1e92b3b4f29ab65b8c1eca7b32c
Rastin Mehr <rastin@anahitapolis.com>
2014-09-24 01:37:41 -0700

    clean up

--- src/site/components/com_base/routers/abstract.php
+++ src/site/components/com_base/routers/abstract.php
@@ -26,28 +26,7 @@
  * @link       http://www.anahitapolis.com
  */
 abstract class ComBaseRouterAbstract extends KObject implements KServiceInstantiatable
-{
-    /**
-     * Force creation of a singleton
-     *
-     * @param   object  An optional KConfig object with configuration options
-     * @param   object  A KServiceInterface object
-     * @return KDispatcherDefault
-     */
-    public static function getInstance(KConfigInterface $config, KServiceInterface $container)
-    { 
-       // Check if an instance with this identifier already exists or not
-        if(!$container->has($config->service_identifier))
-        {
-            //Create the singleton
-            $classname = $config->service_identifier->classname;
-            $instance  = new $classname($config);
-            $container->set($config->service_identifier, $instance);
-        }
-        
-        return $container->get($config->service_identifier);
-    }
-    
+{    
     /**
      * Route patterns
      * 
@@ -90,7 +69,27 @@ abstract class ComBaseRouterAbstract extends KObject implements KServiceInstanti
 
         parent::_initialize($config);
     }
+    
+    /**
+     * Force creation of a singleton
+     *
+     * @param   object  An optional KConfig object with configuration options
+     * @param   object  A KServiceInterface object
+     * @return KDispatcherDefault
+     */
+    public static function getInstance(KConfigInterface $config, KServiceInterface $container)
+    { 
+       // Check if an instance with this identifier already exists or not
+        if(!$container->has($config->service_identifier))
+        {
+            //Create the singleton
+            $classname = $config->service_identifier->classname;
+            $instance  = new $classname($config);
+            $container->set($config->service_identifier, $instance);
+        }
         
+        return $container->get($config->service_identifier);
+    }   
     
     /**
      * Build the route
