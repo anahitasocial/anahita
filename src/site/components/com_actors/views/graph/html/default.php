a7059467ca1fc9df5d2a4eb573d0adb853f91213
Rastin Mehr <rastin@anahitapolis.com>
2014-10-03 20:31:12 -0700

    removed modules

--- src/site/components/com_actors/views/graph/html/default.php
+++ src/site/components/com_actors/views/graph/html/default.php
@@ -1,37 +1,43 @@
 <?php defined('KOOWA') or die; ?>
 
-<?php if($type != 'leadables'): ?>
-<module position="sidebar-b" title="<?= @text('COM-ACTORS-SOCIALGRAPH-STATS') ?>">  
-    <div class="an-socialgraph-stat">
-        <?php if($actor->isFollowable()) : ?>
-        <div class="stat-count">
-            <?= $actor->followerCount ?>
-            <span class="stat-name"><?= @text('COM-ACTORS-SOCIALGRAPH-FOLLOWERS') ?></span>
-        </div>
-        <?php endif; ?>
-        
-        <?php if($actor->isLeadable()) : ?>
-        <div class="stat-count">
-            <?= $actor->leaderCount ?>
-            <span class="stat-name"><?= @text('COM-ACTORS-SOCIALGRAPH-LEADERS') ?></span>
-        </div>        
-        <?php endif; ?>
-        
-        <?php if($actor->isLeadable() && $actor->isFollowable() && $actor->mutualCount) : ?>
-        <div class="stat-count">
-            <?= $actor->mutualCount ?>
-            <span class="stat-name"><?= @text('COM-ACTORS-SOCIALGRAPH-MUTUALS') ?></span>
-        </div>
-        <?php endif; ?>
-    </div>
-</module>
-<?php endif; ?>
-
-<?php if($type == 'leadables'): ?>
-<h3><?= @text('COM-ACTORS-SOCIALGRAPH-FOLLOWERS-ADD-TITLE') ?></h3>
-<?php endif; ?>
-
-<?= @helper('ui.filterbox', @route('view='.@listItemView()->getName().'&layout=list&get=graph&type='.$type.'&id='.$actor->id))?>
+<div class="row">
+	<div class="span6">
+	<?php if($type == 'leadables'): ?>
+	<h3><?= @text('COM-ACTORS-SOCIALGRAPH-FOLLOWERS-ADD-TITLE') ?></h3>
+	<?php endif; ?>
+	
+	<?php if($type != 'leadables'): ?>
+	<h3><?= @text('COM-ACTORS-SOCIALGRAPH-STATS') ?></h3>
+	
+	<div class="an-socialgraph-stat">
+	<?php if($actor->isFollowable()) : ?>
+	<div class="stat-count">
+		<?= $actor->followerCount ?>
+		<span class="stat-name"><?= @text('COM-ACTORS-SOCIALGRAPH-FOLLOWERS') ?></span>
+	</div>
+	<?php endif; ?>
+	        
+	<?php if($actor->isLeadable()) : ?>
+	<div class="stat-count">
+	<?= $actor->leaderCount ?>
+	<span class="stat-name"><?= @text('COM-ACTORS-SOCIALGRAPH-LEADERS') ?></span>
+	</div>        
+	<?php endif; ?>
+	        
+	<?php if($actor->isLeadable() && $actor->isFollowable() && $actor->mutualCount) : ?>
+	<div class="stat-count">
+	<?= $actor->mutualCount ?>
+	<span class="stat-name"><?= @text('COM-ACTORS-SOCIALGRAPH-MUTUALS') ?></span>
+	</div>
+	<?php endif; ?>
+	</div>
+	<?php endif; ?>
+	</div>
+	
+	<div class="span6">
+	<?= @helper('ui.filterbox', @route('view='.@listItemView()->getName().'&layout=list&get=graph&type='.$type.'&id='.$actor->id))?>
+	</div>
+</div>
 
 <div class="an-entities-wrapper">	
 	<?= @template('list') ?>
