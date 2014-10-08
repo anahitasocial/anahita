14659bd4f86287f7dffa01baff46cd5cf941b093
Rastin Mehr <rastin@anahitapolis.com>
2014-10-07 22:38:25 -0700

    added grids, removed module tags

--- packages/Todos/src/site/components/com_todos/views/todo/html/default.php
+++ packages/Todos/src/site/components/com_todos/views/todo/html/default.php
@@ -1,27 +1,31 @@
 <?php defined('KOOWA') or die ?>
 
-<module position="sidebar-b" title="<?= @text('LIB-AN-META') ?>">	
-	<ul class="an-meta">
-		<?php if(isset($todo->editor)) : ?>
-		<li><?= sprintf( @text('LIB-AN-MEDIUM-EDITOR'), @date($todo->updateTime), @name($todo->editor)) ?></li>
+<div class="row">
+	<div class="span8">
+	<?= @helper('ui.header', array()) ?>
+	<?= @template('todo') ?>
+	<?= @helper('ui.comments', $todo, array('pagination'=>true)) ?>
+	</div>
+	
+	<div class="span4">
+		<h4><?= @text('LIB-AN-META') ?></h4>				
+		<ul class="an-meta">
+			<?php if(isset($todo->editor)) : ?>
+			<li><?= sprintf( @text('LIB-AN-MEDIUM-EDITOR'), @date($todo->updateTime), @name($todo->editor)) ?></li>
+			<?php endif; ?>
+			<?php if(!$todo->open) : ?>
+			<li>				
+				<?= sprintf(@text('COM-TODOS-TODO-COMPLETED-BY-REPORT'), @date($todo->openStatusChangeTime), @name($todo->lastChanger)) ?>
+			</li>
+			<?php endif; ?>
+			<li><?= sprintf( @text('LIB-AN-MEDIUM-NUMBER-OF-COMMENTS'), $todo->numOfComments) ?></li>
+		</ul>
+
+		<hr/>
+		
+		<?php if($actor->authorize('administration')): ?>
+		<h4><?= @text('COM-TODOS-TODO-PRIVACY') ?></h4>
+		<?= @helper('ui.privacy',$todo) ?>
 		<?php endif; ?>
-		<?php if(!$todo->open) : ?>
-		<li>				
-			<?= sprintf(@text('COM-TODOS-TODO-COMPLETED-BY-REPORT'), @date($todo->openStatusChangeTime), @name($todo->lastChanger)) ?>
-		</li>
-		<?php endif; ?>
-		<li><?= sprintf( @text('LIB-AN-MEDIUM-NUMBER-OF-COMMENTS'), $todo->numOfComments) ?></li>
-	</ul>
-</module>
-
-<?php if($actor->authorize('administration')): ?>
-<module position="sidebar-b" title="<?= @text('COM-TODOS-TODO-PRIVACY') ?>">
-	<?= @helper('ui.privacy',$todo) ?>
-</module>
-<?php else: ?>
-<module position="sidebar-b" style="none"></module>
-<?php endif; ?>
-
-<?= @template('todo') ?>
-
-<?= @helper('ui.comments', $todo, array('pagination'=>true)) ?>
+	</div>
+</div>
