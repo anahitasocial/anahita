232d8d5ec87c689b69196b49cbdc3731f0983a21
Rastin Mehr <rastin@anahitapolis.com>
2014-10-08 17:26:05 -0700

    Removed module tags and added grid

--- packages/Invites/src/site/components/com_invites/views/connections/html/default.php
+++ packages/Invites/src/site/components/com_invites/views/connections/html/default.php
@@ -1,25 +1,17 @@
 <?php defined('KOOWA') or die('Restricted access');?>
 
-<?php if ( !isset($service) ) : ?>
-<?= @template('add') ?>
-<?php return;?>
-<?php endif; ?>
+<?php if(isset($service)) : ?>
 <script data-inline src="http://connect.facebook.net/en_US/all.js"></script>
 <script data-inline src="media://com_invites/js/facebook.js"></script>
 
 <div id="fb-root"></div>
 
-<?php
-    $url = @route()->getURl(KHttpUrl::SCHEME | KHttpUrl::HOST | KHttpUrl::PORT );    
-?>
-
 <script>
-
 <?php
 $subject = htmlspecialchars(sprintf(@text('COM-INVITES-MESSAGE-SUBJECT'), JFactory::getConfig()->getValue('sitename')));
-$body    = @helper('text.script', sprintf(@text('COM-INVITES-MESSAGE-BODY'), @name($viewer, false), JFactory::getConfig()->getValue('sitename')));
+$body = @helper('text.script', sprintf(@text('COM-INVITES-MESSAGE-BODY'), @name($viewer, false), JFactory::getConfig()->getValue('sitename')));
+$url = @route()->getURl(KHttpUrl::SCHEME | KHttpUrl::HOST | KHttpUrl::PORT );
 ?>
-
 new FacebookInvite({
 	'appId'    :  <?= $service->getAppID() ?>,
     'subject'  : '<?= $subject ?>',
@@ -27,25 +19,35 @@ new FacebookInvite({
     'appURL'   : '<?= $url ?>',
     'picture'  : '<?= $viewer->getPortraitURL() ?>',
 });
-
 </script>
 
-<a href="#" data-trigger="Invite" class="btn btn-primary">
-    + <?= @text('COM-INVITES-ACTION-FB-INVITE') ?>
-</a>  
 <style>
-#block {
+#block 
+{
     display:none;
 }
 </style>
-<module position="sidebar-b" style="none"></module>	
-<div class="an-entities-wrapper">	
-<?php 
-$controller = @service('com://site/people.controller.person', array('request'=>array('view'=>'people')));                
-$controller->getState()->setList($items);
-?>
-<?= $controller->getView()->layout('list')->display() ?>
+
+<div class="row">
+	<div class="span8">
+		<?= @helper('ui.header', array()) ?>
+	
+		<a href="#" data-trigger="Invite" class="btn btn-primary">
+    		+ <?= @text('COM-INVITES-ACTION-FB-INVITE') ?>
+		</a>
+	
+        <div class="an-entities-wrapper">	
+        <?php 
+        $controller = @service('com://site/people.controller.person', array('request'=>array('view'=>'people')));                
+        $controller->getState()->setList($items);
+        ?>
+        <?= $controller->getView()->layout('list')->display() ?>
+        </div>
+	</div>
 </div>
+<?php else: ?>
+<?= @template('add') ?>
+<?php endif; ?>
 
 
 
--- packages/Subscriptions/src/site/components/com_subscriptions/views/signup/html/processed.php
+++ packages/Subscriptions/src/site/components/com_subscriptions/views/signup/html/processed.php
@@ -2,18 +2,20 @@
 
 <?= @template('_steps', array('current_step'=>'processed')) ?>
 
-<module position="sidebar-b" style="none"></module>
-
-<div class="alert alert-success alert-block">
-	<p><?= @text('COM-SUB-THANK-YOU') ?></p>
-	<?php if ( !$viewer->guest() ) : ?>
-	<p><a class="btn" href="<?= @route('view=subscription') ?>"><?= @text('COM-SUB-VIEW-YOUR-SUBSCRIPTION') ?></a></p>
-	<?php endif;?>
+<div class="row">
+	<div class="span8">	
+        <div class="alert alert-success alert-block">
+        	<p><?= @text('COM-SUB-THANK-YOU') ?></p>
+        	<?php if ( !$viewer->guest() ) : ?>
+        	<p><a class="btn" href="<?= @route('view=subscription') ?>"><?= @text('COM-SUB-VIEW-YOUR-SUBSCRIPTION') ?></a></p>
+        	<?php endif;?>
+        </div>
+        
+        <?php if ( $viewer->guest() ) : ?>
+        <form action="<?@route()?>" method="post">
+            <input type="hidden" name="_action" value="login" />
+            <input type="submit" class="btn" value="Login">
+        </form>
+        <?php endif; ?>
+	</div>
 </div>
-
-<?php if ( $viewer->guest() ) : ?>
-<form action="<?@route()?>" method="post">
-    <input type="hidden" name="_action" value="login" />
-    <input type="submit" class="btn" value="Login">
-</form>
-<?php endif; ?>
