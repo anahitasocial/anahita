521517f0f86722e2e6cfc9df118a7042ed1d413f
Rastin Mehr <rastin@anahitapolis.com>
2014-10-11 14:16:11 -0700

    added header

--- src/site/components/com_medium/views/media/html/default.php
+++ src/site/components/com_medium/views/media/html/default.php
@@ -1,6 +1,8 @@
 <?php defined('KOOWA') or die; ?>
 
-<?= @helper('ui.filterbox',@route('layout=list')) ?>
+<?= @helper('ui.header', array()) ?>
+
+<?= @helper('ui.filterbox', @route('layout=list')) ?>
 
 <div class="an-entities-wrapper">
 	<?= @template('list') ?>
