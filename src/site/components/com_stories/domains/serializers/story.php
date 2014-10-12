b00a5eed4724d80f161177a199a93fa65ff6ba89
Rastin Mehr <rastin@anahitapolis.com>
2014-10-12 14:05:32 -0700

    return story id in serialized (aka json) story object

--- src/site/components/com_stories/domains/serializers/story.php
+++ src/site/components/com_stories/domains/serializers/story.php
@@ -34,51 +34,46 @@ class ComStoriesDomainSerializerStory extends ComBaseDomainSerializerDefault
     {        
         $data = array();
         $data['objectType'] = 'com.'.$entity->getIdentifier()->package.'.'.$entity->getIdentifier()->name;
+        $data['id'] = $entity->id;
         $data['component'] = $entity->component;
         $data['name'] = $entity->name;  
+    
+        $items = $entity->subject;
         
-        //$data['subject'] = $entity->subject->toSerializableArray();
-        //$data['target']  = $entity->target->toSerializableArray();
-        
-//        if ( $entity->aggregated() ) 
-//        {
-            $items = $entity->subject;
-            
-            if(is_array($items))
-            {         
-            	foreach($items as $item)
-                	$data['subjects'][] = $item->toSerializableArray();
-            } 
-            else 
-            {
-                $data['subject'] = $items->toSerializableArray();
-            }
+        if(is_array($items))
+        {         
+        	foreach($items as $item)
+            	$data['subjects'][] = $item->toSerializableArray();
+        } 
+        else 
+        {
+            $data['subject'] = $items->toSerializableArray();
+        }
 
-            $items = $entity->target;
-            
-            if ( is_array($items) )
-            {         
-            	foreach($items as $item)
-                	$data['targets'][] = $item->toSerializableArray(); 
-            }
-            elseif( $items ) 
-            {
-                $data['target'] = $items->toSerializableArray();
-            }
-            
-            $items = $entity->object;
-            
-            if(is_array($items))
-            {        
-           		foreach($items as $item)
-                	$data['objects'][] = $item->toSerializableArray();
-            }
-            elseif($items) 
-            {
-                $data['object'] = $items->toSerializableArray();
-            }
-//        }
+        $items = $entity->target;
         
+        if ( is_array($items) )
+        {         
+        	foreach($items as $item)
+            	$data['targets'][] = $item->toSerializableArray(); 
+        }
+        elseif( $items ) 
+        {
+            $data['target'] = $items->toSerializableArray();
+        }
+        
+        $items = $entity->object;
+        
+        if(is_array($items))
+        {        
+       		foreach($items as $item)
+            	$data['objects'][] = $item->toSerializableArray();
+        }
+        elseif($items) 
+        {
+            $data['object'] = $items->toSerializableArray();
+        }
+    
         $data['creationTime'] = $entity->creationTime->getDate();        
 
         if($entity->getIdentifier()->name == 'story')
