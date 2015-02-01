e90bdd39f5e2fc1c394bf85ed76038fd7ead1480
Rastin Mehr <rastin@anahitapolis.com>
2015-01-31 18:27:15 -0800

    Used document add script method

--- src/site/templates/base/html/tmpl/js.php
+++ src/site/templates/base/html/tmpl/js.php
@@ -1,9 +1,18 @@
+<?php defined('KOOWA') or die; ?>
+
+<?php $document =& JFactory::getDocument(); ?>
+<?php $path = JURI::root(true).DS.'media'.DS.'lib_anahita'.DS.'js'.DS.'production'.DS; ?>
+
 <?php if(defined('JDEBUG') && JDEBUG ) : ?>
-	<?php @helper('javascript.combine', array(
-  				'file'   => JPATH_ROOT.'/media/lib_anahita/js/site.js',
-  				'output' => JPATH_ROOT.'/media/lib_anahita/js/production/site.uncompressed.js'
-  			)) ?>
-<script src="media://lib_anahita/js/production/site.uncompressed.js"></script>
+
+<?php 
+@helper('javascript.combine', array(
+    'file'   => JPATH_ROOT.'/media/lib_anahita/js/site.js',
+  	'output' => JPATH_ROOT.'/media/lib_anahita/js/production/site.uncompressed.js'
+)); 
+
+$document->addScript($path.'site.uncompressed.js');    		
+?>
 <?php else : ?>
-<script src="media://lib_anahita/js/production/site.js"></script>
+<?php $document->addScript($path.'site.js'); ?>
 <?php endif; ?>
