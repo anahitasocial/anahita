b45d13515f64ae9a850c401f9c206abeba45683e
Rastin Mehr <rastin@anahitapolis.com>
2015-02-01 20:17:28 -0800

    organize action plugins in a directory

---
+++ src/media/lib_anahita/js/anahita/actions/vote.js
@@ -0,0 +1,70 @@
+/**
+ * Author: Rastin Mehr
+ * Email: rastin@anahitapolis.com
+ * Copyright 2015 rmdStudio Inc. www.rmdStudio.com
+ * License: GPL3
+ */
+
+;(function ($, window, document) {
+	
+	'use strict';
+	
+	$.fn.vote = function(type) {
+		
+		type = type || '';
+		
+		var elem = $(this);
+		
+		var voteCountWrapper = $('#vote-count-wrapper-' + elem.data('nodeid'));
+		
+		$.ajax({
+			type : 'POST',
+			url : elem.attr('href'),
+			data : {
+				action : elem.data('action')
+			},
+			beforeSend : function(){
+				elem.fadeTo('fast', 0.3);
+			}.bind(elem),
+			success : function(response){
+				
+				if(elem.data('action') == ('vote' + type))
+				{
+					elem.data('action', 'unvote' + type);
+					elem.text(StringLibAnahita.action.unvote);
+				}	
+				else if(elem.data('action') == ('unvote' + type))
+				{
+					elem.data('action', 'vote' + type);
+					elem.text(StringLibAnahita.action.vote);
+				}	
+				
+				elem.toggleClass('action-vote' + type).toggleClass('action-unvote' + type);
+				elem.fadeTo('fast', 1);
+				voteCountWrapper.html(response);
+					
+			}.bind(elem)
+		});
+	};
+	
+	$('body').on('click', 'a.action-vote', function( event ) {
+		event.preventDefault();
+		$(this).vote();
+	});
+	
+	$('body').on('click', 'a.action-unvote', function( event ) {
+		event.preventDefault();
+		$(this).vote();
+	});
+	
+	$('body').on('click', 'a.action-votecomment', function( event ) {
+		event.preventDefault();
+		$(this).vote('comment');
+	});
+	
+	$('body').on('click', 'a.action-unvotecomment', function( event ) {
+		event.preventDefault();
+		$(this).vote('comment');
+	});
+	
+}(jQuery, window, document));
