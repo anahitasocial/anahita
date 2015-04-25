f9afbf7281a5cf1c2ff805a29fce93182bcabf3b
Rastin Mehr <rastin@anahitapolis.com>
2015-04-25 14:37:44 -0700

    comments, photos, sets, and notes use KFliterString to filter content.

--- src/site/components/com_notes/filters/post.php
+++ src/site/components/com_notes/filters/post.php
@@ -22,24 +22,4 @@
  * @license    GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
  * @link       http://www.GetAnahita.com
  */
-class ComNotesFilterPost extends ComMediumFilterPost
-{
-    /**
-    * Initializes the default configuration for the object
-    *
-    * Called from {@link __construct()} as a first step of object instantiation.
-    *
-    * @param KConfig $config An optional KConfig object with configuration options.
-    *
-    * @return void
-    */
-    protected function _initialize(KConfig $config)
-    {
-        $config->append(array(
-            'tag_list'   => array(),
-            'tag_method' => 0
-        ));   
-
-        parent::_initialize($config);
-    }
-}
+class ComNotesFilterPost extends KFilterString{}
