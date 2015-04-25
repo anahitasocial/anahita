e7732c0fb0064e5b33700ea75262b6cf07077993
Rastin Mehr <rastin@anahitapolis.com>
2015-04-24 23:41:49 -0700

    added utf-8 charset to the templates

--- src/site/templates/base/html/error.php
+++ src/site/templates/base/html/error.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-  		<meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1.0, user-scalable=0, charset=utf-8">
+  		<meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1.0, user-scalable=0" charset="UTF-8">
         <?= @render('favicon') ?>
         <?= @render('style') ?>
   	</head>
