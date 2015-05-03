06e0dab11a4285db79daa500c1b452e234b595eb
Rastin Mehr <rastin@anahitapolis.com>
2015-05-02 21:08:37 -0700

    some changes on the photo set layout

--- packages/Photos/src/site/components/com_photos/views/set/html/default.php
+++ packages/Photos/src/site/components/com_photos/views/set/html/default.php
@@ -17,29 +17,11 @@
     <?= @helper('ui.comments', $set) ?>
 	</div>
 	
-	<div class="span4">	
+	<div class="span4 visible-desktop">	
 			
-    	<div class="an-entity an-photos-set editable" data-url="<?= @route($set->getURL()) ?>">		
-    		<div id="set-cover-wrapper">
-    			<?= @template('cover') ?>
-    		</div>
-    		
-    		<div class="entity-description-wrapper">
-        		<h3 class="entity-title">
-        			<?= @escape( $set->title ) ?>
-        		</h3>
-        		
-        		<div class="entity-description">
-        			<?= @content( nl2br( $set->description ), array( 'exclude' => array('gist','video') ) ) ?>
-        		</div>
-			</div>
-    		
-    		<div class="entity-meta">
-    			<div class="an-meta" id="vote-count-wrapper-<?= $set->id ?>">
-    			<?= @helper('ui.voters', $set); ?>
-    			</div>
-    		</div>
-    	</div>
+    	<div id="set-cover-wrapper">
+                <?= @template('cover') ?>
+            </div>
     	
     	<h4 class="block-title">
     	    <?= @text('LIB-AN-META') ?>
--- packages/Photos/src/site/components/com_photos/views/set/html/set.php
+++ packages/Photos/src/site/components/com_photos/views/set/html/set.php
@@ -2,6 +2,24 @@
 
 <?php @commands('toolbar') ?>
 
+<div class="an-entity an-photos-set editable" data-url="<?= @route($set->getURL()) ?>">             
+    <div class="entity-description-wrapper">
+        <h3 class="entity-title">
+            <?= @escape( $set->title ) ?>
+        </h3>
+        
+        <div class="entity-description">
+            <?= @content( nl2br( $set->description ), array( 'exclude' => array('gist','video') ) ) ?>
+        </div>
+    </div>
+    
+    <div class="entity-meta">
+        <div class="an-meta" id="vote-count-wrapper-<?= $set->id ?>">
+        <?= @helper('ui.voters', $set); ?>
+        </div>
+    </div>
+</div>
+
 <?php if( $set->authorize('edit') ) : ?>
 <div id="photo-selector"></div>
 <?php endif; ?>
