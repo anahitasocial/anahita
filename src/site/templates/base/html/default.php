b2fc185c2e846011de164b80ba10ca0a4afc1fd2
Rastin Mehr <rastin@anahitapolis.com>
2015-05-28 01:03:07 -0700

    removed extra space

--- src/site/templates/base/html/default.php
+++ src/site/templates/base/html/default.php
@@ -2,7 +2,6 @@
 <!DOCTYPE html>
 <html>
 	<head>
-	    
   		<meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1.0, user-scalable=0" charset="UTF-8">
   	    
   	    <?= @render('favicon') ?>
