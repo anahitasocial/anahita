eb259f1b246c4986c98f0cdf03bfa8647ba50582
Rastin Mehr <rastin@anahitapolis.com>
2015-06-21 15:05:54 -0700

    data migration and installation updates

--- src/administrator/components/com_anahita/schemas/migrations/11.php
+++ src/administrator/components/com_anahita/schemas/migrations/11.php
@@ -20,13 +20,22 @@ class ComAnahitaSchemaMigration11 extends ComMigratorMigrationVersion
     */
     public function up()
     {
+        //delete open social table    
         dbexec('DROP TABLE #__opensocial_profiles');    
+        
+        //delete legacy com_cache and com_opensocial
         dbexec('DELETE FROM #__components WHERE `option` = \'com_cache\' OR `option` = \'com_opensocial\' ');
+        
+        //some clean up
         dbexec('DELETE FROM #__components WHERE name = "" ');
+        
+        //fix broken menu link
         dbexec('UPDATE #__components SET `admin_menu_link` = \'option=com_mailer\' WHERE `option` = \'com_mailer\' ');
         
+        //Delete legacy plugins
         dbexec('DELETE FROM #__plugins WHERE `element` IN ( \'ptag\', \'syntax\', \'opensocial\', \'mtupgrade\', \'usertype\' ) ');
         
+        //add pinned field to the nodes table
         dbexec('ALTER TABLE jos_anahita_nodes ADD `pinned` TINYINT(1) NOT NULL DEFAULT \'0\' AFTER `enabled`');
     
         //add github gist plugin
@@ -34,6 +43,12 @@ class ComAnahitaSchemaMigration11 extends ComMigratorMigrationVersion
         
         //remove the photo plugin
         dbexec("DELETE FROM #__plugins WHERE `element` = 'photo' ");
+        
+        //remove the migrator_migrations table if exists
+        dbexec('DROP TABLE #__migrator_migrations');
+        
+        //some of the plugins are core plugins
+        dbexec("UPDATE jos_plugins SET iscore = 1 WHERE element IN ('joomla', 'gist', 'medium', 'link', 'video', 's3')");
     }
 
    /**
