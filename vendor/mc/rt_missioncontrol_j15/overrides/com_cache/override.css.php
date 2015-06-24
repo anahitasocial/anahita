963c3e3231fbdb60fdaf7a9ddfbc6c1294fdf91c
Rastin Mehr <rastin@anahitapolis.com>
2015-06-23 21:26:01 -0700

    legacy files

--- vendor/mc/rt_missioncontrol_j15/overrides/com_cache/override.css.php
+++
@@ -1,6 +0,0 @@
-<?php header("Content-type: text/css"); 
-//this sets up the colors for the core missioncontrol template
-//require('../../css/color-vars.php');
-?>
-.option-com-cache.task-purgeadmin table.adminlist td,
-.option-com-cache.task-purge table.adminlist td {padding:20px 10px;}
