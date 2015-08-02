3326b741b2919a87eed03c11b1ed3f42735c87ce
Rastin Mehr <rastin@anahitapolis.com>
2015-08-01 18:48:15 -0700

    new migration script to drop the legacy ACL tables and fields. This migration file isn't finale and more queries will be added.

---
+++ src/administrator/components/com_anahita/schemas/migrations/13.php
@@ -0,0 +1,54 @@
+<?php
+
+/** 
+ * LICENSE: ##LICENSE##
+ * 
+ * @package    Com_Anahita
+ * @subpackage Schema_Migration
+ */
+
+/**
+ * Schema Migration
+ *
+ * @package    Com_Anahita
+ * @subpackage Schema_Migration
+ */
+class ComAnahitaSchemaMigration13 extends ComMigratorMigrationVersion
+{
+   /**
+    * Called when migrating up
+    */
+    public function up()
+    {
+       //migrate users usertype    
+       dbexec('UPDATE #__users SET `usertype`=\'registered\' WHERE `usertype` NOT LIKE \'%admin%\' ');
+       dbexec('UPDATE #__users SET `usertype`=\'administrator\' WHERE `usertype`=\'Administrator\' ');
+       dbexec('UPDATE #__users SET `usertype`=\'super-administrator\' WHERE `usertype`=\'Super Administrator\' '); 
+       dbexec('ALTER TABLE #__users DROP COLUMN `gid` ');
+       dbexec('ALTER TABLE #__users DROP COLUMN `sendEmail` ');
+       
+       //migrate people usertype
+       dbexec('UPDATE #__anahita_nodes SET `person_usertype`=\'registered\' WHERE `person_usertype` NOT LIKE \'%admin%\' ');
+       dbexec('UPDATE #__anahita_nodes SET `person_usertype`=\'administrator\' WHERE `person_usertype`=\'Administrator\' ');
+       dbexec('UPDATE #__anahita_nodes SET `person_usertype`=\'super-administrator\' WHERE `person_usertype`=\'Super Administrator\' ');
+       
+       //migrate session table
+       dbexec('ALTER TABLE #__session DROP COLUMN `gid` ');
+       
+       //drop legacy tables
+       dbexec('DROP TABLE #__core_acl_aro');
+       dbexec('DROP TABLE #__core_acl_aro_groups');
+       dbexec('DROP TABLE #__core_acl_aro_map');
+       dbexec('DROP TABLE #__core_acl_aro_sections');
+       dbexec('DROP TABLE #__core_acl_groups_aro_map');
+       dbexec('DROP TABLE #__groups');
+    }
+
+   /**
+    * Called when rolling back a migration
+    */        
+    public function down()
+    {
+        //add your migration here        
+    }
+}
