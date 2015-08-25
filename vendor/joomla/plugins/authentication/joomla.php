b3f3f567458b7c0ee86f35e8821856215dfe8a71
Rastin Mehr <rastin@anahitapolis.com>
2015-08-24 22:20:44 -0700

    removed legacy code

--- vendor/joomla/plugins/authentication/joomla.php
+++ vendor/joomla/plugins/authentication/joomla.php
@@ -28,22 +28,6 @@ class plgAuthenticationJoomla extends JPlugin
 {
 
     /**
-     * Constructor
-     *
-     * For php4 compatability we must not use the __constructor as a constructor for plugins
-     * because func_get_args ( void ) returns a copy of all passed arguments NOT references.
-     * This causes problems with cross-referencing necessary for the observer design pattern.
-     *
-     * @param object $subject The object to observe
-     * @param array  $config  An array that holds the plugin configuration
-     * @since 1.5
-     */
-    function plgAuthenticationJoomla(& $subject, $config) 
-    {
-        parent::__construct($subject, $config);
-    }
-
-    /**
      * This method should handle any authentication and report back to the subject
      *
      * @access  public
