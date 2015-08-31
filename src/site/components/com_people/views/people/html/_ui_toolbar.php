2039f25f24377d4b5e1c5575d872dc6bf1b09f69
Rastin Mehr <rastin@anahitapolis.com>
2015-08-31 00:18:20 -0700

    Started implementing the people view filters for admins

---
+++ src/site/components/com_people/views/people/html/_ui_toolbar.php
@@ -0,0 +1,27 @@
+<?php defined('KOOWA') or die ?>
+
+<?php if (get_viewer()->admin()) : ?>
+<div class="row">
+    <div class="span12">
+        <form action="https://localhost/anahitapolis/rmdstudio/www/people?layout=list" id="an-filterbox" class="form-inline" name="an-filterbox" method="get">             
+            <input placeholder="Filter..." type="text" name="q" class="input-large search-query" id="an-search-query" value="" size="21" maxlength="21">    
+        
+            <?php
+            $usertypes = array(
+                '' => JText::_('COM-PEOPLE-FILTER-USERTYPE'),
+                ComPeopleDomainEntityPerson::USERTYPE_REGISTERED => @text('COM-PEOPLE-USERTYPE-REGISTERED'),
+                ComPeopleDomainEntityPerson::USERTYPE_ADMINISTRATOR => @text('COM-PEOPLE-USERTYPE-ADMINISTRATOR'),
+                ComPeopleDomainEntityPerson::USERTYPE_SUPER_ADMINISTRATOR => @text('COM-PEOPLE-USERTYPE-SUPER-ADMINISTRATOR')
+            ); 
+            
+            $html = $this->getService('com:base.template.helper.html');
+            ?>    
+            <?= $html->select('filter[usertype]', array('options'=>$usertypes)) ?>
+            
+            <label class="checkbox">
+                <input type="checkbox" name="filter[disabled]"> <?= @text('COM-PEOPLE-FILTER-DISABLED') ?>
+            </label>    
+        </form>
+    </div>
+</div>
+<?php endif; ?>
