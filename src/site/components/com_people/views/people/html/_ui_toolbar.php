192752891a6c8ef427b1b922731390921bf2b6bc
Rastin Mehr <rastin@anahitapolis.com>
2015-08-31 00:20:21 -0700

    only hide some filters from registered users

--- src/site/components/com_people/views/people/html/_ui_toolbar.php
+++ src/site/components/com_people/views/people/html/_ui_toolbar.php
@@ -1,11 +1,11 @@
 <?php defined('KOOWA') or die ?>
 
-<?php if (get_viewer()->admin()) : ?>
+
 <div class="row">
     <div class="span12">
         <form action="https://localhost/anahitapolis/rmdstudio/www/people?layout=list" id="an-filterbox" class="form-inline" name="an-filterbox" method="get">             
-            <input placeholder="Filter..." type="text" name="q" class="input-large search-query" id="an-search-query" value="" size="21" maxlength="21">    
-        
+            <input placeholder="Filter..." type="text" name="q" class="input-large search-query" id="an-search-query" value="" size="21" maxlength="21" />    
+            <?php if (get_viewer()->admin()) : ?>
             <?php
             $usertypes = array(
                 '' => JText::_('COM-PEOPLE-FILTER-USERTYPE'),
@@ -13,15 +13,14 @@
                 ComPeopleDomainEntityPerson::USERTYPE_ADMINISTRATOR => @text('COM-PEOPLE-USERTYPE-ADMINISTRATOR'),
                 ComPeopleDomainEntityPerson::USERTYPE_SUPER_ADMINISTRATOR => @text('COM-PEOPLE-USERTYPE-SUPER-ADMINISTRATOR')
             ); 
-            
             $html = $this->getService('com:base.template.helper.html');
             ?>    
             <?= $html->select('filter[usertype]', array('options'=>$usertypes)) ?>
-            
             <label class="checkbox">
-                <input type="checkbox" name="filter[disabled]"> <?= @text('COM-PEOPLE-FILTER-DISABLED') ?>
-            </label>    
+                <input type="checkbox" name="filter[disabled]"> 
+                <?= @text('COM-PEOPLE-FILTER-DISABLED') ?>
+            </label>  
+            <?php endif; ?>  
         </form>
     </div>
 </div>
-<?php endif; ?>
