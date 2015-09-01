8dc1d71ecb1e837b8028109ce5f2b105cebc7387
Rastin Mehr <rastin@anahitapolis.com>
2015-09-01 00:40:11 -0700

    updated filter box layouts

--- src/site/components/com_base/templates/helpers/ui/_ui_filterbox.php
+++ src/site/components/com_base/templates/helpers/ui/_ui_filterbox.php
@@ -1,5 +1,5 @@
 <?php defined('KOOWA') or die; ?>
 
-<form action="<?= $search_action ?>" id="an-filterbox" class="an-filterbox" name="an-filterbox"  method="get">				
+<form action="<?= $search_action ?>" id="an-filterbox" class="form-inline" name="an-filterbox"  method="get">				
 	<input placeholder="<?= @text('LIB-AN-FILTER-PLACEHOLDER') ?>" type="text" name="q" class="input-large search-query" id="an-search-query" value="<?= empty($value) ? '' : $value ?>" size="21" maxlength="21" />
 </form>
