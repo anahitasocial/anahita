3bf80faff0ea27f3ed6c2805b27677fb79390cc9
Rastin Mehr <rastin@anahitapolis.com>
2015-09-02 00:39:00 -0700

    not being used atm

--- src/site/components/com_people/domains/queries/person.php
+++ src/site/components/com_people/domains/queries/person.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Person Query 
+ * Person Query
  *
  * @category   Anahita
  * @package    Com_Search
@@ -9,16 +9,23 @@
  * @license    GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
  * @link       http://www.GetAnahita.com
  */
-class ComPeopleDomainQueryPerson extends AnDomainQueryDefault 
+class ComPeopleDomainQueryPerson extends AnDomainQueryDefault
 {
     /**
      * Build the filter query
-     * 
+     *
      * @return void
      */
     protected function _beforeQuerySelect()
     {
-        
-    }  
+        /*
+        if($this->filter_usertype) {
+            $this->where('person.person_usertype', '=', $this->filter_usertype);
+        }
+
+        if($this->filter_disabled_accounts) {
+            $this->where('person.enabled', '=', 0);
+        }
+        */
+    }
 }
-    
