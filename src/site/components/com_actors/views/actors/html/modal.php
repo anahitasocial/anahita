21418637d9ca713d16c269deb4fa6a156263eb31
Rastin Mehr <rastin@anahitapolis.com>
2015-09-04 23:50:05 -0700

    legacy code clean up

--- src/site/components/com_actors/views/actors/html/modal.php
+++ src/site/components/com_actors/views/actors/html/modal.php
@@ -3,25 +3,25 @@
 </div>
 
 <div class="modal-body">
-    
+
 <div class="an-entities" id="an-entities-main">
 	<div id="an-actors" class="an-entities">
 		<?php foreach($items as $item ) : ?>
-            <div class="an-entity an-record dropdown-actions <?= $highlight ?>">
+            <div class="an-entity">
             	<div class="clearfix">
             		<div class="entity-portrait-square">
             			<?= @avatar($item) ?>
             		</div>
-            		
+
             		<div class="entity-container">
             			<h3 class="entity-name">
             				<?= @name($item) ?>
             			</h3>
-            	
+
             			<div class="entity-meta">
             			<?= $item->followerCount ?>
-            			<span class="stat-name"><?= @text('COM-ACTORS-SOCIALGRAPH-FOLLOWERS') ?></span> 
-            			
+            			<span class="stat-name"><?= @text('COM-ACTORS-SOCIALGRAPH-FOLLOWERS') ?></span>
+
             			<?php if($item->isLeadable()): ?>
             			/ <?= $item->leaderCount ?>
             			<span class="stat-name"><?= @text('COM-ACTORS-SOCIALGRAPH-LEADERS') ?></span>
@@ -29,7 +29,7 @@
             			</div>
             		</div>
             	</div>
-            </div>			
+            </div>
 		<?php endforeach; ?>
 	</div>
 </div>
