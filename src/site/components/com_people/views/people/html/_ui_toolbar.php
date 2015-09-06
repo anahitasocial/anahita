26381b4b3b9bd81ff0609b4a5a0653ed3ad4fe04
Rastin Mehr <rastin@anahitapolis.com>
2015-09-05 20:15:54 -0700

    increased the limit on the filter box

--- src/site/components/com_people/views/people/html/_ui_toolbar.php
+++ src/site/components/com_people/views/people/html/_ui_toolbar.php
@@ -1,7 +1,7 @@
 <?php defined('KOOWA') or die ?>
 
-<form action="<?= @route('layout=list') ?>" id="an-filterbox" class="an-filterbox form-inline" name="an-filterbox" method="get">             
-    <input placeholder="Filter..." type="text" name="q" class="input-large search-query" id="an-search-query" value="" size="21" maxlength="21" />    
+<form action="<?= @route('layout=list') ?>" id="an-filterbox" class="an-filterbox form-inline" name="an-filterbox" method="get">
+    <input placeholder="Filter..." type="text" name="q" class="input-large search-query" id="an-search-query" value="" size="21" maxlength="100" />    
     <?php if (get_viewer()->admin()) : ?>
     <?php
     $usertypes = array(
@@ -11,11 +11,11 @@
         ComPeopleDomainEntityPerson::USERTYPE_SUPER_ADMINISTRATOR => @text('COM-PEOPLE-USERTYPE-SUPER-ADMINISTRATOR'),
     );
     $html = $this->getService('com:base.template.helper.html');
-    ?>    
+    ?>
     <?= $html->select('filter[usertype]', array('options' => $usertypes)) ?>
     <label class="checkbox">
-        <input type="checkbox" name="filter[disabled]"> 
+        <input type="checkbox" name="filter[disabled]">
         <?= @text('COM-PEOPLE-FILTER-DISABLED') ?>
-    </label>  
-    <?php endif; ?>  
+    </label>
+    <?php endif; ?>
 </form>
