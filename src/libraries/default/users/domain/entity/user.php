3724295355986178f614484d35d46754c95f1484
Rastin Mehr <rastin@anahitapolis.com>
2015-09-12 13:59:22 -0700

    clean up

--- src/libraries/default/users/domain/entity/user.php
+++ src/libraries/default/users/domain/entity/user.php
@@ -1,20 +1,5 @@
 <?php
 
-/** 
- * LICENSE: ##LICENSE##.
- * 
- * @category   Anahita
- *
- * @author     Arash Sanieyan <ash@anahitapolis.com>
- * @author     Rastin Mehr <rastin@anahitapolis.com>
- * @copyright  2008 - 2010 rmdStudio Inc./Peerglobe Technology Inc
- * @license    GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
- *
- * @version    SVN: $Id$
- *
- * @link       http://www.GetAnahita.com
- */
-
 /**
  * User object.
  *
@@ -51,7 +36,7 @@ class LibUsersDomainEntityUser extends AnDomainEntityDefault
 
     /**
      * Automatically sets the activation token for the user.
-     * 
+     *
      * @return LibUsersDomainEntityUser
      */
     public function requiresActivation()
