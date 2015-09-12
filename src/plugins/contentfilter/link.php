8159ec80f61dac40e2c6b590871bb561811d025e
Rastin Mehr <rastin@anahitapolis.com>
2015-09-12 15:06:39 -0700

    Exclude parentheses when parsing hyperlinks

--- src/plugins/contentfilter/link.php
+++ src/plugins/contentfilter/link.php
@@ -1,17 +1,4 @@
-<?php 
-
-/**
- * LICENSE: ##LICENSE##
- *
- * @category   Anahita
- * @package    Plg_Contentfilter
- * @author     Arash Sanieyan <ash@anahitapolis.com>
- * @author     Rastin Mehr <rastin@anahitapolis.com>
- * @copyright  2008 - 2010 rmdStudio Inc./Peerglobe Technology Inc
- * @license    GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
- * @version    SVN: $Id$
- * @link       http://www.GetAnahita.com
- */
+<?php
 
 /**
  * Creates a hyperlink from the URLs
@@ -31,7 +18,7 @@ class PlgContentfilterLink extends PlgContentfilterAbstract
      * Called from {@link __construct()} as a first step of object instantiation.
      *
      * @param KConfig $config An optional KConfig object with configuration options.
-     * 
+     *
      * @return void
      */
     protected function _initialize(KConfig $config)
@@ -42,21 +29,21 @@ class PlgContentfilterLink extends PlgContentfilterAbstract
 
         parent::_initialize($config);
     }
-    	
+
 	/**
-	 * Filter a value. 
-	 * 
+	 * Filter a value.
+	 *
 	 * @param string The text to filter
-	 * 
+	 *
 	 * @return string
-	 */	
+	 */
 	public function filter($text)
 	{
-    	$text = preg_replace( 
-    	"/((?<!=\")(http|ftp)+(s)?:\/\/[^<>\s]+)/i", 
-    	"<a href=\"\\0\" target=\"_blank\">\\0</a>", 
+    	$text = preg_replace(
+    	"/((?<!=\")(http|ftp)+(s)?:\/\/[^<>()\s]+)/i",
+    	"<a href=\"\\0\" target=\"_blank\">\\0</a>",
     	$text);
-		
-		return $text;	
+
+		return $text;
 	}
 }
