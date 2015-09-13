8ff1c09547e9c7c1e9c4e81038ce012c9da8b491
Rastin Mehr <rastin@anahitapolis.com>
2015-09-13 01:41:08 -0700

    authentication was removed by mistake. It is back

--- src/plugins/system/anahita.php
+++ src/plugins/system/anahita.php
@@ -1,8 +1,8 @@
 <?php
 
-/** 
+/**
  * LICENSE: ##LICENSE##
- * 
+ *
  * @category   Anahita
  * @package    Plugins
  * @author     Arash Sanieyan <ash@anahitapolis.com>
@@ -17,7 +17,7 @@ jimport('joomla.plugin.plugin');
 
 /**
  * Anahita System Plugin
- * 
+ *
  * @category   Anahita
  * @package    Plg
  * @author     Arash Sanieyan <ash@anahitapolis.com>
@@ -25,32 +25,32 @@ jimport('joomla.plugin.plugin');
  * @license    GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
  * @link       http://www.GetAnahita.com
  */
-class PlgSystemAnahita extends JPlugin 
-{        
+class PlgSystemAnahita extends JPlugin
+{
 	/**
 	 * Constructor
-	 * 
+	 *
 	 * @param mixed $subject Dispatcher
 	 * @param array $config  Array of configuration
-     * 
+     *
      * @return void
 	 */
 	public function __construct($subject, $config = array())
-	{	    
+	{
         // Command line fixes for Joomla
-        if (PHP_SAPI === 'cli') 
+        if (PHP_SAPI === 'cli')
         {
             if (! isset($_SERVER['HTTP_HOST']))
             {
                 $_SERVER['HTTP_HOST'] = '';
             }
-            
+
             if (! isset($_SERVER['REQUEST_METHOD']))
             {
                 $_SERVER['REQUEST_METHOD'] = '';
             }
-        }                
-        
+        }
+
         // Check for suhosin
         if (in_array('suhosin', get_loaded_extensions()))
         {
@@ -62,14 +62,14 @@ class PlgSystemAnahita extends JPlugin
             {
                 $url =  KService::get('application')->getRouter()->getBaseUrl();
                 $url.= '/templates/system/error_suhosin.html';
-                
+
                 KService::get('application.dispatcher')->getResponse()->setRedirect($url);
                 KService::get('application.dispatcher')->getResponse()->send();
-                
+
                 return;
             }
         }
-        
+
         //Safety Extender compatibility
         if (extension_loaded('safeex') && strpos('tmpl', ini_get('safeex.url_include_proto_whitelist')) === false)
         {
@@ -78,13 +78,13 @@ class PlgSystemAnahita extends JPlugin
             ini_set('safeex.url_include_proto_whitelist', $whitelist);
         }
 
-        if (! 
-              JFactory::getApplication()->getCfg('caching') || 
+        if (!
+              JFactory::getApplication()->getCfg('caching') ||
               (
-                  JFactory::getUser()->usertype == ComPeopleDomainEntityPerson::USERTYPE_SUPER_ADMINISTRATOR && 
+                  JFactory::getUser()->usertype == ComPeopleDomainEntityPerson::USERTYPE_SUPER_ADMINISTRATOR &&
                   KRequest::get('get.clearapc', 'cmd')
               )
-           )  
+           )
         {
             //clear apc cache for components
             //@NOTE If apc is shared across multiple services
@@ -97,49 +97,49 @@ class PlgSystemAnahita extends JPlugin
             $jconfig = new JConfig();
             clean_apc_with_prefix(md5($jconfig->secret).'-cache-');
         }
-        
+
 		KService::get('plg:storage.default');
         JFactory::getLanguage()->load('overwrite', JPATH_ROOT);
 		JFactory::getLanguage()->load('lib_anahita', JPATH_ROOT);
-        
+
         parent::__construct($subject, $config);
 	}
-	
+
     /**
      * Remebers handling
-     * 
+     *
      * @return void
      */
     public function onAfterInitialise()
     {
-        global $mainframe; 
-           
+        global $mainframe;
+
         $viewer = get_viewer();
-        
+
         if(!$viewer->enabled){
-            KService::get('com://site/people.helper.person')->logout();    
+            KService::get('com://site/people.helper.person')->logout();
         }
 
         // No remember me for admin
         if ($mainframe->isAdmin()){
-            return;  
+            return;
         }
-        
+
         if ($viewer->guest()){
             return;
         }
-        
+
         jimport('joomla.utilities.utility');
         jimport('joomla.utilities.simplecrypt');
-        
+
         $user = array();
         $remember = JUtility::getHash('JLOGIN_REMEMBER');
-        
+
         // for json requests obtain the username and password from the $_SERVER array
         // else if the remember me cookie exists, decrypt and obtain the username and password from it
         if (
-               KRequest::has('server.PHP_AUTH_USER') && 
-               KRequest::has('server.PHP_AUTH_PW') && 
+               KRequest::has('server.PHP_AUTH_USER') &&
+               KRequest::has('server.PHP_AUTH_PW') &&
                KRequest::format() == 'json'
            )
         {
@@ -147,8 +147,8 @@ class PlgSystemAnahita extends JPlugin
             $user['password'] = KRequest::get('server.PHP_AUTH_PW', 'raw');
         }
         elseif (isset($_COOKIE[$remember]) && $_COOKIE[$remember] != '')
-        {          	
-            $key = JUtility::getHash(KRequest::get('server.HTTP_USER_AGENT', 'raw'));    
+        {
+            $key = JUtility::getHash(KRequest::get('server.HTTP_USER_AGENT', 'raw'));
 
             if($key)
             {
@@ -157,14 +157,20 @@ class PlgSystemAnahita extends JPlugin
             	$user = (array) @unserialize($cookie);
             }
         }
-        
-        if (! empty($user)) 
-        { 
+
+        if (! empty($user))
+        {
         	try
             {
-                KService::get('com://site/people.helper.person')->login($user, true);
+								jimport('joomla.user.authentication');
+								$authentication =& JAuthentication::getInstance();
+								$authResponse = $authentication->authenticate($user, array());
+
+								if($authResponse->status === JAUTHENTICATE_STATUS_SUCCESS) {
+										KService::get('com://site/people.helper.person')->login($user, true);
+								}
             }
-            catch (RuntimeException $e) 
+            catch (RuntimeException $e)
             {
                 //only throws exception if we are using JSON format
                 //otherwise let the current app handle it
@@ -174,10 +180,10 @@ class PlgSystemAnahita extends JPlugin
                 }
             }
         }
-        
+
         return;
     }
-    
+
 	/**
 	 * store user method
 	 *
@@ -191,8 +197,8 @@ class PlgSystemAnahita extends JPlugin
 	public function onAfterStoreUser($user, $isnew, $succes, $msg)
 	{
 		return true;
-	}	
-    
+	}
+
 	/**
 	 * delete user method
 	 *
@@ -201,19 +207,19 @@ class PlgSystemAnahita extends JPlugin
 	 * @param 	array		holds the user data
 	 */
 	public function onBeforeDeleteUser($user)
-	{	
+	{
 		$person = KService::get('repos://site/people.person')->find(array('userId'=>$user['id']));
-	    
+
 	    if ($person)
 	    {
 	        KService::get('repos://site/components')
 	        ->fetchSet()
 	        ->registerEventDispatcher(KService::get('anahita:event.dispatcher'));
-	        
+
 	        KService::get('anahita:event.dispatcher')
 	        ->dispatchEvent('onDeleteActor', array('actor_id'=>$person->id));
-            
-	        $person->delete()->save();  	           
+
+	        $person->delete()->save();
 	    }
 	}
 }
