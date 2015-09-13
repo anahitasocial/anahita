75aaea84488ba6ca8c368bce8b37c2a55145b901
Rastin Mehr <rastin@anahitapolis.com>
2015-09-13 01:49:18 -0700

    clean up

--- src/libraries/default/people/viewer.php
+++ src/libraries/default/people/viewer.php
@@ -1,23 +1,8 @@
 <?php
 
-/** 
- * LICENSE: ##LICENSE##.
- * 
- * @category   Anahita
- *
- * @author     Arash Sanieyan <ash@anahitapolis.com>
- * @author     Rastin Mehr <rastin@anahitapolis.com>
- * @copyright  2008 - 2010 rmdStudio Inc./Peerglobe Technology Inc
- * @license    GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
- *
- * @version    SVN: $Id$
- *
- * @link       http://www.GetAnahita.com
- */
-
 /**
  * A Factory class to return the current viewer person object.
- * 
+ *
  * @category   Anahita
  *
  * @author     Arash Sanieyan <ash@anahitapolis.com>
@@ -36,32 +21,25 @@ class LibPeopleViewer extends KObject implements KServiceInstantiatable
      *
      * @return KServiceInstantiatable
      */
-    public static function getInstance(KConfigInterface $config, KServiceInterface $container)
-    {
-        if (!$container->has($config->service_identifier)) {
-            $id = JFactory::getUser()->id;
+     public static function getInstance(KConfigInterface $config, KServiceInterface $container)
+     {
+         if (!$container->has($config->service_identifier)) {
 
-            if (!$id) {
-                $viewer = $container->get('repos://site/people.person')
-                ->getEntity()
-                ->setData(array(
-                    'userType' => ComPeopleDomainEntityPerson::USERTYPE_GUEST, ),
-                    AnDomain::ACCESS_PROTECTED
-                );
-                $viewer->set('id', 0);
-                $viewer->getRepository()->extract($viewer);
-            } else {
-                $viewer = $container
-                          ->get('repos://site/people.person')
-                          ->find(array('userId' => $id));
-            }
+             $id = JFactory::getUser()->id;
 
-            $container->set(
-                $config->service_identifier,
-                $viewer
-                );
-        }
+             if (!$id) {
+                 $viewer = $container->get('repos://site/people.person')
+                 ->getEntity()
+                 ->setData(array('userType'=>'Guest'), AnDomain::ACCESS_PROTECTED);
+                 $viewer->set('id',0);
+                 $viewer->getRepository()->extract($viewer);
+             } else {
+                 $viewer = $container->get('repos://site/people.person')->find(array('userId' => $id));
+             }
 
-        return $container->get($config->service_identifier);
-    }
+             $container->set($config->service_identifier, $viewer);
+         }
+
+         return $container->get($config->service_identifier);
+     }
 }
