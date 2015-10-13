f8cdc8825bcbd96f88e33e0075386bfcae80beb6
Rastin Mehr <rastin@anahitapolis.com>
2015-10-13 12:24:54 -0700

    removed return parameter

--- src/site/components/com_people/views/person/html/signup.php
+++ src/site/components/com_people/views/person/html/signup.php
@@ -4,28 +4,25 @@
 <?php $user = @service('repos://site/users')->getQuery(true)->fetchValue('id'); ?>
 
 <div class="row">
-	<div class="offset3 span6">	
-        
+	<div class="offset3 span6">
+
         <?php if (!$user): ?>
         <div class="alert alert-info alert-block">
             <h4><?= @text('COM-PEOPLE-WELCOME1') ?></h4>
             <p><?= @text('COM-PEOPLE-WELCOME2') ?></p>
         </div>
-        <?php endif; ?>	
-        
+        <?php endif; ?>
+
 		<?php $return = empty($return) ? null : $return; ?>
 
         <form action="<?= @route('view=person') ?>" method="post" name="person-form" id="person-form" class="well" autocomplete="off">
             <input type="hidden" name="action" value="add" />
-            <?php if ($return) : ?>
-            <input type="hidden" name="return" value="<?= $return; ?>" />
-            <?php endif; ?>
-        
+
             <fieldset>
                 <legend>
                     <?= @text('COM-PEOPLE-ACTION-CREATE-AN-ACCOUNT') ?>
                 </legend>
-        
+
                 <div class="control-group">
                     <label class="control-label"  for="person-name">
                         <?= @text('COM-PEOPLE-SIGNUP-NAME'); ?>
@@ -34,7 +31,7 @@
                         <input class="input-block-level" type="text" id="person-name" name="name" maxlength="25" minlength="6" required />
                     </div>
                 </div>
-                
+
                 <div class="control-group">
                     <label class="control-label"  for="person-username">
                         <?= @text('COM-PEOPLE-SIGNUP-USERNAME'); ?>
@@ -44,7 +41,7 @@
                         <input data-validate="username" data-url="<?= @route('view=person', false) ?>" type="text" id="person-username" class="input-block-level" name="username" pattern="<?= $usernamePattern ?>" maxlength="100" minlength="6" required />
                     </div>
                 </div>
-                        
+
                 <div class="control-group">
                     <label class="control-label"  for="person-email">
                         <?= @text('COM-PEOPLE-SIGNUP-EMAIL'); ?>
@@ -54,17 +51,17 @@
                        <input data-validate="email" data-url="<?= @route('view=person', false) ?>" type="email" name="email" pattern="<?= $emailPattern ?>" id="person-email" class="input-block-level" maxlength="100" minlength="10" required  />
                     </div>
                 </div>
-                        
+
                 <div class="control-group">
                     <label class="control-label"  for="password">
                         <?= @text('COM-PEOPLE-SIGNUP-PASSWORD'); ?>
                     </label>
                     <div class="controls">
-                        <?= @helper('password.input') ?>               
+                        <?= @helper('password.input') ?>
                     </div>
                 </div>
             </fieldset>
-                    
+
             <div class="form-actions">
                 <button type="submit" class="btn btn-primary btn-large pull-right" data-loading-text="<?= @text('LIB-AN-ACTION-PLEASE-WAIT') ?>">
                     <?= @text('COM-PEOPLE-ACTION-REGISTER') ?>
