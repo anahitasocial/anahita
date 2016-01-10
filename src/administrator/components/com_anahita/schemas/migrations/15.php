9f0c926d626d7aeb059c608b570b74a61091c8d4
Rastin Mehr <rastin@anahitapolis.com>
2016-01-10 13:47:02 -0800

    Fixed bug in the query

--- src/administrator/components/com_anahita/schemas/migrations/15.php
+++ src/administrator/components/com_anahita/schemas/migrations/15.php
@@ -21,15 +21,15 @@ class ComAnahitaSchemaMigration15 extends ComMigratorMigrationVersion
     public function up()
     {
       //Create new fields for com_locations
-      dbexec("ALTER TABLE jos_nodes ADD `address` VARCHAR(255) DEFAULT NULL AFTER `geo_longitude`");
-      dbexec("ALTER TABLE jos_nodes ADD `city` VARCHAR(50) DEFAULT NULL AFTER `address`");
-      dbexec("ALTER TABLE jos_nodes ADD `state_province` VARCHAR(50) DEFAULT NULL AFTER `city`");
-      dbexec("ALTER TABLE jos_nodes ADD `country` VARCHAR(30) DEFAULT NULL AFTER `state_province`");
-      dbexec("ALTER TABLE jos_nodes ADD `postalcode` VARCHAR(15) DEFAULT NULL AFTER `country`");
+      dbexec("ALTER TABLE `#__nodes` ADD `address` VARCHAR(255) DEFAULT NULL AFTER `geo_longitude`");
+      dbexec("ALTER TABLE `#__nodes` ADD `city` VARCHAR(50) DEFAULT NULL AFTER `address`");
+      dbexec("ALTER TABLE `#__nodes` ADD `state_province` VARCHAR(50) DEFAULT NULL AFTER `city`");
+      dbexec("ALTER TABLE `#__nodes` ADD `country` VARCHAR(30) DEFAULT NULL AFTER `state_province`");
+      dbexec("ALTER TABLE `#__nodes` ADD `postalcode` VARCHAR(15) DEFAULT NULL AFTER `country`");
 
       //add extension records
-      dbexec("INSERT INTO `#__components` (`name`, `link`, `admin_menu_link`, `admin_menu_alt`, `admin_menu_image`, `option`,`iscore`, `enabled`) VALUES ('Locations', 'option=com_locations', 'option=com_locations', 'Locations', 'js/ThemeOffice/component.png', 'com_locations', 1, 1)");
-      dbexec("INSERT INTO `#__plugins` (`name`, `element`, `folder`, `published`, `iscore`) VALUES ('Content Filter - Location', 'location', 'contentfilter', 1, 1)");
+      dbexec("INSERT INTO `#__components` (`name`, `link`, `admin_menu_link`, `admin_menu_alt`, `admin_menu_img`, `option`,`iscore`, `enabled`) VALUES ('Locations', 'option=com_locations', 'option=com_locations', 'Locations', 'js/ThemeOffice/component.png', 'com_locations', 1, 1)");
+
     }
 
    /**
