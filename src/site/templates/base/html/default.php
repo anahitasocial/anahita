2c906d1746d0de98fd33dbc35980bc679ee71277
Rastin Mehr <rastin@anahitapolis.com>
2016-01-24 18:58:47 -0800

    CSS fixes

--- src/site/templates/base/html/default.php
+++ src/site/templates/base/html/default.php
@@ -3,28 +3,28 @@
 <html>
 	<head>
   		<meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1.0, user-scalable=0" charset="UTF-8">
-  	    
+
   	    <?= @render('favicon') ?>
   	    <?= @render('style') ?>
   	</head>
-    <body>	  	
+    <body>
     	<?= @template('tmpl/modal') ?>
-    	
+
         <div id="container-system-message" class="container">
         	<?= @render('messages') ?>
-        </div>    		
+        </div>
 
         <?= @template('tmpl/navbar') ?>
-        
+
         <div class="container">
         <?= $this->getView()->content; ?>
         </div>
-        
+
         <?php if ($this->getView()->getParams()->poweredby): ?>
         <div class="container">
         	<div class="row">
         		<div class="span12">
-        			<p class="muted">
+        			<p class="poweredby muted">
         				<em>Powered by <a href="http://www.GetAnahita.com" target="_blank" rel="nofollow">Anahita</a></em>
         			</p>
         		</div>
