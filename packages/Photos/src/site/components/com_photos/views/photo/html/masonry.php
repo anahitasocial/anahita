a71c495106709b3054699efec55b9f30d85fc70b
Rastin Mehr <rastin@anahitapolis.com>
2016-02-20 01:37:07 -0800

    bug fix

--- packages/Photos/src/site/components/com_photos/views/photo/html/masonry.php
+++ packages/Photos/src/site/components/com_photos/views/photo/html/masonry.php
@@ -23,7 +23,7 @@
 
 	<div class="entity-portrait-medium">
 		<?php $caption = htmlspecialchars($photo->title, ENT_QUOTES, 'UTF-8'); ?>
-		<a data-rel="media-photos-<?= $photo->id ?>" data-trigger="MediaViewer" title="<?= $caption ?>" href="<?= $photo->getPortraitURL('original') ?>">			
+		<a data-rel="media-photos-<?= $photo->id ?>" data-trigger="MediaViewer" title="<?= $caption ?>" href="<?= $photo->getPortraitURL('original') ?>">
 			<img alt="<?= @escape($photo->title) ?>" src="<?= $photo->getPortraitURL('medium') ?>" />
 		</a>
 	</div>
@@ -63,9 +63,7 @@
 		</div>
 	</div>
 
-	<?php if ($photo->authorize('edit')) : ?>
 	<div class="entity-actions">
 		<?= @helper('ui.commands', @commands('list')) ?>
 	</div>
-	<?php endif; ?>
 </div>
