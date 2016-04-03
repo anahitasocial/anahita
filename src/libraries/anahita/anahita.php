0045bf7e050d037ae10984beef25d9173e265ec6
Rastin Mehr <rastin@anahitapolis.com>
2016-04-03 00:58:38 -0700

    updated version number

--- src/libraries/anahita/anahita.php
+++ src/libraries/anahita/anahita.php
@@ -26,7 +26,7 @@ class anahita
      *
      * @var string
      */
-    protected static $_version = '4.2.0';
+    protected static $_version = '4.2.1';
 
     /**
      * Path to Anahita libraries.
