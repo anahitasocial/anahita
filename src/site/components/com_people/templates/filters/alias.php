ecf6284afc50eaeedaf74658b334a62b19ef7add
Rastin Mehr <rastin@anahitapolis.com>
2016-04-03 20:09:29 -0700

    implementing com_settings

--- src/site/components/com_people/templates/filters/alias.php
+++ src/site/components/com_people/templates/filters/alias.php
@@ -11,7 +11,7 @@
  *
  * @link       http://www.GetAnahita.com
  */
-class ComSettingsTemplateFilterAlias extends LibBaseTemplateFilterAlias
+class ComPeopleTemplateFilterAlias extends LibBaseTemplateFilterAlias
 {
     /**
      * Constructor.
