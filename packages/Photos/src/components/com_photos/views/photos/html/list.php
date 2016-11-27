2d61534ec9a8223f86364984680165a9bee078b2
Rastin Mehr <rastin@timble.net>
2016-11-27 03:01:27 -0800

    using only masonry layout for photo lists

--- packages/Photos/src/components/com_photos/views/photos/html/list.php
+++ packages/Photos/src/components/com_photos/views/photos/html/list.php
@@ -1,13 +1,5 @@
-<? defined('KOOWA') or die('Restricted access');?>	
+<? defined('KOOWA') or die('Restricted access');?>
 
-<div class="an-entities" id="an-entities-main">
-<? if (count($photos)) : ?>
-	<? foreach ($photos as $photo) : ?>
-	<?= @view('photo')->layout('list')->photo($photo)->filter($filter) ?>
-	<? endforeach; ?>
-<? else: ?>
-	<?= @message(@text('LIB-AN-NODES-EMPTY-LIST-MESSAGE')) ?>
-<? endif; ?>
-</div>
-
-<?= @pagination($photos, array('url' => @route('layout=list'))) ?>
+<? foreach ($photos as $photo) : ?>
+<?= @view('photo')->layout('list')->photo($photo)->filter($filter) ?>
+<? endforeach; ?>
