317370b3417defe1d628bb00a9f5f1eb640f1f18
Rastin Mehr <rastin@timble.net>
2017-04-05 01:19:53 -0700

    prepared version 4.3.4

--- src/libraries/anahita/anahita.php
+++ src/libraries/anahita/anahita.php
@@ -27,7 +27,7 @@ class anahita
      *
      * @var string
      */
-    protected static $_version = '4.3.3';
+    protected static $_version = '4.3.4';
 
     /**
      * Path to Anahita libraries.
