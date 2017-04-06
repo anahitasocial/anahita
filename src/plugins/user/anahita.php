db61e7073bcce33d7e83a10c7a4ebe5413412cf9
Rastin Mehr <rastin@timble.net>
2017-04-05 20:45:17 -0700

    clean up

--- src/plugins/user/anahita.php
+++ src/plugins/user/anahita.php
@@ -3,95 +3,95 @@
 class plgUserAnahita extends PlgAnahitaDefault
 {
 
-	/**
-	* Is called before a person is added or edited
-	*
-	* @param $event->data
-	* @return boolean
-	*/
-	public function onBeforeSavePerson(KEvent $event)
-	{
-		return true;
-	}
+    /**
+    * Is called before a person is added or edited
+    *
+    * @param $event->data
+    * @return boolean
+    */
+    public function onBeforeSavePerson(KEvent $event)
+    {
+        return true;
+    }
 
-	/**
-	* Is called before a person is added or edited
-	*
-	* @param $event->person
-	* @return boolean
-	*/
-	public function onAfterSavePerson(KEvent $event)
-	{
-		return true;
-	}
+    /**
+    * Is called before a person is added or edited
+    *
+    * @param $event->person
+    * @return boolean
+    */
+    public function onAfterSavePerson(KEvent $event)
+    {
+        return true;
+    }
 
-	/**
+    /**
      * Is called before a person id deleted
      *
-	 * @param $event->data
- 	 * @return boolean
+     * @param $event->data
+     * @return boolean
      */
     public function onBeforeDeletePerson(KEvent $event)
     {
-		return true;
+        return true;
     }
 
-	/**
+    /**
      * Is called before a person id deleted
      *
-	 * @param $event->person
- 	 * @return boolean
+     * @param $event->person
+     * @return boolean
      */
-	public function onAfterDeletePerson(KEvent $event)
-	{
-		return true;
-	}
+    public function onAfterDeletePerson(KEvent $event)
+    {
+        return true;
+    }
 
-	/**
-	 * Is called when a person logs in
-	 *
-	 * @param $event->credentials
- 	 * @return boolean
-	 */
-	public function onLoginPerson(KEvent $event)
-	{
-		$credentials = $event->credentials;
-		$options = $event->options;
+    /**
+     * Is called when a person logs in
+     *
+     * @param $event->credentials
+     * @return boolean
+     */
+    public function onLoginPerson(KEvent $event)
+    {
+        $credentials = $event->credentials;
+        $options = $event->options;
 
-		$person = KService::get('repos:people.person')->find(array(
-						'username' => $credentials->username,
-						'enabled' => 1
-					));
+        $person = KService::get('repos:people.person')->find(array(
+                        'username' => $credentials->username,
+                        'enabled' => 1
+                    ));
 
-		if (!isset($person)) {
-			$msg = "Did not find a user with username: ".$credentials->username;
-			throw new AnErrorException($msg, KHttpResponse::UNAUTHORIZED);
-			return false;
-		}
+        if (!isset($person)) {
+            $msg = "Did not find a user with username: ".$credentials->username;
+            throw new AnErrorException($msg, KHttpResponse::UNAUTHORIZED);
+            return false;
+        }
 
-		$person->visited();
-		$session = KService::get('com:sessions');
-		$session->set('person', (object) $person->getData());
+        $person->visited();
+        $session = KService::get('com:sessions');
+        $session->set('person', (object) $person->getData());
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
-	 * Is called when a person logs out
-	 *
-	 * @param $event->person
- 	 * @return boolean
-	 */
-	public function onLogoutPerson(KEvent $event)
-	{
-		$person = $event->person;
+    /**
+     * Is called when a person logs out
+     *
+     * @param $event->person
+     * @return boolean
+     */
+    public function onLogoutPerson(KEvent $event)
+    {
+        $person = $event->person;
 
-		if ($person->id === 0) {
-			return false;
-    	}
+        if ($person->id === 0) {
+            return false;
+        }
 
-		KService::get('repos:sessions.session')->destroy(array('nodeId' => $person->id));
+        KService::get('repos:sessions.session')->destroy(array('nodeId' => $person->id));
 
-		return KService::get('com:sessions')->destroy();
-	}
+        return KService::get('com:sessions')->destroy();
+    }
 }
