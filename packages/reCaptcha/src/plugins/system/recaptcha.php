a97ed9ad4ff87e88bbb12e1cd35019d2ec814545
Rastin Mehr <rastin@timble.net>
2017-10-11 00:11:27 -0700

    #451 - Implement Google reCAPTCHA as a plugin - using a system plugin now to verify reCaptcha
    NEXT: need to build a behaviour which can be added to the forms that use reCaptcha

--- packages/reCaptcha/src/plugins/system/recaptcha.php
+++ packages/reCaptcha/src/plugins/system/recaptcha.php
@@ -16,20 +16,72 @@ class plgSystemRecaptcha extends PlgAnahitaDefault
     /**
      * onAfterRender handler.
      */
-    public function onAfterRoute()
+    public function onAfterRoute(KEvent $event)
     {
-        $option = KRequest::get('get.option', 'string', null);
-        $view = KRequest::get('get.option', 'string', null);
-        $email = KRequest::get('post.email', 'email', null);
-        $username = KRequest::get('post.email', 'username', null);
-
-        if ($option == 'com_people' && $view == 'person' && $mail != null && $username != null) {
-            $this->verifyResponse($event->data->get('g-recaptcha-response'));
-            die('here');
-    	}
+        if (KRequest::method() === 'POST' && $this->_hasRecaptcha()) {
+            $recaptchaResponse = KRequest::get('post.g-recaptcha-response', 'string', null);
+            if (! $this->_verifyResponse($recaptchaResponse)) {
+                throw new KException("Unauthorized Request", 403);
+                return;
+            }
+        }
     }
 
-    private function verifyResponse($recaptchaResponse)
+    /**
+     * onAfterRender handler.
+     */
+    public function onAfterDispatch(KEvent $event)
+    {
+
+    }
+
+    /**
+     * onAfterRender handler.
+     */
+    public function onAfterRender(KEvent $event)
+    {
+
+    }
+
+    /**
+    *   Returns true if the request is coming from a form with reCaptcha.
+    *   The request is from one of the following forms:
+    *   1. login form
+    *   2. registration form
+    *   3. add group form
+    *
+    *   @return boolean 
+    */
+    private function _hasRecaptcha()
+    {
+        $option = KRequest::get('get.option', 'string', '');
+        $view = KRequest::get('get.view', 'cmd', '');
+        $id = KRequest::get('get.view', 'int', 0);
+
+        $action = KRequest::get('post.action', 'cmd', 'add');
+
+        if ($option === 'com_people') {
+            if ($view === 'session' && $action === 'add') {
+                return true;
+            }
+            if ($view === 'person' && $id === 0) {
+                return true;
+            }
+        }
+
+        if ($option === 'com_groups' && $view === 'group' && $id === 0 && $action === 'add') {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+    *   Verifies the request reCaptcha token against the reCaptcha API
+    *
+    *   @return boolean
+    */
+    private function _verifyResponse($recaptchaResponse)
     {
         $data = http_build_query(array(
                 'secret' => $this->_params->get('secret-key'),
@@ -48,11 +100,6 @@ class plgSystemRecaptcha extends PlgAnahitaDefault
         $response = file_get_contents('https://www.google.com/recaptcha/api/siteverify', false, $context);
         $result = json_decode($response);
 
-        if(! $result->success) {
-          error_log("loggin failed");
-          throw new LibBaseControllerExceptionUnauthorized('Recaptcha Failed');
-        }
-
-        return true;
+        return (bool) $result->success;
     }
 }
