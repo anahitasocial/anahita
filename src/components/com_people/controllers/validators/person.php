54c1a48f7d096bb02edbc808a95c37d5487dc7a0
Rastin Mehr <rastin@timble.net>
2018-05-02 18:15:12 -0700

    removed viewers. it was a bad idea

--- src/components/com_people/controllers/validators/person.php
+++ src/components/com_people/controllers/validators/person.php
@@ -22,10 +22,9 @@ class ComPeopleControllerValidatorPerson extends LibBaseControllerValidatorDefau
      */
     public function validateEmail($email)
     {
-        $viewer = $this->getService('com:people.viewer');
         $person = $this->getService('repos:people.person')->find(array('email' => $email));
 
-        if ($person && !$person->eql($viewer)) {
+        if ($person) {
             $this->setMessage('Email is already in use');
             return false;
         }
@@ -42,10 +41,9 @@ class ComPeopleControllerValidatorPerson extends LibBaseControllerValidatorDefau
      */
     public function validateUsername($username)
     {
-        $viewer = $this->getService('com:people.viewer');
         $person = $this->getService('repos:people.person')->find(array('username' => $username));
 
-        if ($person && !$person->eql($viewer)) {
+        if ($person) {
             $this->setMessage('Username is already in use');
             return false;
         }
