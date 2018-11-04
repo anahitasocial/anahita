a3a1cd6cd11c56136ad8aa591537ff6a5aa5b4d9
Rastin Mehr <rastin@timble.net>
2018-11-03 19:02:16 -0700

    updated version number

--- src/libraries/anahita/anahita.php
+++ src/libraries/anahita/anahita.php
@@ -27,7 +27,7 @@ class anahita
      *
      * @var string
      */
-    protected static $_version = '4.3.12';
+    protected static $_version = '4.3.13';
 
     /**
      * Path to Anahita libraries.
