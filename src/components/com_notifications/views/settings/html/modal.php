ce9d15dca00bd8e06fb3d3d9bfd318c8f2e0d2a7
rmdstudio <rastin@rmdstudio.com>
2021-01-02 22:44:45 -0800

    Notifications - added: an option to mute email notifications - fixed: spelling in language file

--- src/components/com_notifications/views/settings/html/modal.php
+++ src/components/com_notifications/views/settings/html/modal.php
@@ -12,20 +12,24 @@
     </select>
 </form>
 <?php endif; ?>
-<?php
-    $setting = @service('repos:notifications.setting')->findOrAddNew(array(
-        'person' => $viewer,
-        'actor' => $actor,
-    ))->reset();
-?>
-<form action="<?= @route('option=com_notifications&view=setting&oid='.$actor->id)?>" method="post">
-	<label class="control-label"><?= @text('COM-NOTIFICATIONS-ACTOR-SEND-EMAIL')?></label>
-	<label class="checkbox">
-		<input class="autosubmit" name="email" <?= $setting->getValue('posts', 1) == 2 ? 'disabled="true"' : ''?> <?= $setting->sendEmail('posts', 1) && $setting->getValue('posts', 1) < 2 ? 'checked' : ''?> value="1" type="checkbox" />
-		<?= $viewer->email?>
-		<a href="<?=@route($viewer->getURL().'&get=settings&edit=account')?>"><?= @text('COM-NOTIFICATIONS-ACTOR-NOTIFICATION-CHANGE-EMAIL') ?></a>
-	</label>
-</form>
+
+<? $muteEmail = (bool) get_config_value('notifications.mute_email') ?>
+<? if (!$muteEmail) : ?>
+	<?php
+	    $setting = @service('repos:notifications.setting')->findOrAddNew(array(
+	        'person' => $viewer,
+	        'actor' => $actor,
+	    ))->reset();
+	?>
+	<form action="<?= @route('option=com_notifications&view=setting&oid='.$actor->id)?>" method="post">
+		<label class="control-label"><?= @text('COM-NOTIFICATIONS-ACTOR-SEND-EMAIL')?></label>
+		<label class="checkbox">
+			<input class="autosubmit" name="email" <?= $setting->getValue('posts', 1) == 2 ? 'disabled="true"' : ''?> <?= $setting->sendEmail('posts', 1) && $setting->getValue('posts', 1) < 2 ? 'checked' : ''?> value="1" type="checkbox" />
+			<?= $viewer->email?>
+			<a href="<?=@route($viewer->getURL().'&get=settings&edit=account')?>"><?= @text('COM-NOTIFICATIONS-ACTOR-NOTIFICATION-CHANGE-EMAIL') ?></a>
+		</label>
+	</form>
+<? endif ?>
 </mbody>
 
 <mfooter>
