6c57832e17fbadf4bc9e8b2b6748735072a1953e
rmdstudio <rastin@rmdstudio.com>
2021-06-07 00:14:56 -0700

    Cleaned Locations

--- src/components/com_locations/templates/helpers/ui/_ui_map_google.php
+++
@@ -1,9 +0,0 @@
-<? defined('ANAHITA') or die('Restricted access') ?>
-
-<? if (defined('ANDEBUG') && ANDEBUG) : ?>
-<script src="com_locations/js/map.google.js" />
-<? else: ?>
-<script src="com_locations/js/min/map.google.min.js" />
-<? endif; ?>
-
-<div class="an-map" data-locations="<?= $locations ?>"></div>
