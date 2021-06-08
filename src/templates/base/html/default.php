0456245023ddcd40d59cc78958502503f4aa8ea4
rmdstudio <rastin@rmdstudio.com>
2021-06-07 17:27:08 -0700

    nohtml - removed: content filters - removed: template base css

--- src/templates/base/html/default.php
+++ src/templates/base/html/default.php
@@ -3,36 +3,8 @@
 <html>
 	<head>
   		<meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1.0, user-scalable=0" charset="UTF-8">
-
-  	    <?= @render('favicon') ?>
-  	    <?= @render('style') ?>
-  	</head>
+  		<?= @render('favicon') ?>
+	</head>
     <body>
-    	<?= @template('tmpl/modal') ?>
-
-        <div id="container-system-message" class="container">
-        	<?= @render('messages') ?>
-        </div>
-
-        <?= @template('tmpl/navbar') ?>
-	
-        <div class="container">
-        <?= $this->getView()->content; ?>
-        </div>
-
-        <? if ($this->getView()->getParams()->poweredby): ?>
-        <div class="container">
-        	<div class="row">
-        		<div class="span12">
-        			<p class="poweredby muted">
-        				<em>Powered by <a href="https://www.GetAnahita.com" target="_blank" rel="nofollow">Anahita</a></em>
-        			</p>
-        		</div>
-        	</div>
-        </div>
-        <? endif; ?>
-
-        <?= @template('tmpl/js') ?>
-        <?= @template('tmpl/analytics') ?>
     </body>
 </html>
--- src/templates/base/html/error.php
+++ src/templates/base/html/error.php
@@ -4,16 +4,10 @@
     <head>
   		<meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1.0, user-scalable=0" charset="UTF-8">
         <?= @render('favicon') ?>
-        <?= @render('style') ?>
   	</head>
     <body>
-        <?= @template('tmpl/navbar') ?>
-
         <div class="container">
     	<?= $this->getView()->content; ?>
     	</div>
-
-    	<?= @template('tmpl/js') ?>
-        <?= @template('tmpl/analytics') ?>
     </body>
 </html>
