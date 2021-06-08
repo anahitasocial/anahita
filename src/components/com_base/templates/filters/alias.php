c5db1f223191e619cba48608143915fee51ac6c6
rmdstudio <rastin@rmdstudio.com>
2021-06-07 22:36:57 -0700

    Cleaned com_base

--- src/components/com_base/templates/filters/alias.php
+++ src/components/com_base/templates/filters/alias.php
@@ -24,23 +24,9 @@ class ComBaseTemplateFilterAlias extends LibBaseTemplateFilterAlias
         parent::__construct($config);
 
         $this->_alias_read = array_merge($this->_alias_read, array(
-            '@flash_message(' => '$this->renderHelper(\'ui.flash\',',
-            '@flash_message' => '$this->renderHelper(\'ui.flash\')',
-            '@commands(' => '$this->getHelper(\'toolbar\')->commands(',
-            '@content(' => 'PlgContentfilterChain::getInstance()->filter(',
-            '@pagination(' => '$this->renderHelper(\'ui.pagination\',',
-            '@infinitescroll(' => '$this->renderHelper(\'ui.infinitescroll\',',
             '@avatar(' => '$this->renderHelper(\'com:actors.template.helper.avatar\',',
             '@cover(' => '$this->renderHelper(\'com:actors.template.helper.cover\',',
             '@name(' => '$this->renderHelper(\'com:actors.template.helper.name\',',
-            '@editor(' => '$this->renderHelper(\'ui.editor\',',
-            '@message(' => '$this->renderHelper(\'ui.message\',',
-            '@date(' => '$this->renderHelper(\'date.format\',',
-            '@searchbox(' => '$this->renderHelper(\'ui.search\',',
-            '@location(' => '$this->renderHelper(\'com:locations.template.helper.ui.location\',',
-            '@map(' => '$this->renderHelper(\'com:locations.template.helper.ui.map\',',
-            '@map_api(' => '$this->renderHelper(\'com:locations.template.helper.ui.api\',',
-            '@map_api_nearby(' => '$this->renderHelper(\'com:locations.template.helper.ui.nearby\',',
         ));
     }
 }
