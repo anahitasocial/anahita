826f7e59e3bddb558f0a2322ec68e68431eab704
rmdstudio <rastin@rmdstudio.com>
2022-02-23 22:08:20 -0800

    Anahita Mail - added: phpPath of Mamp for testing purpose only

--- src/components/com_notifications/domains/repositories/notification.php
+++ src/components/com_notifications/domains/repositories/notification.php
@@ -69,8 +69,9 @@ class ComNotificationsDomainRepositoryNotification extends AnDomainRepositoryDef
             $command = sprintf('php -d max_execution_time=60 %s/index.php %s id=%d >/dev/null 2>&1', ANPATH_BASE, PROCESSOR_PATH, $context->entity->id);
             shell_exec($command);
             
-            // Use this for testing only    
-            // $command = sprintf('php %s/index.php %s id=%d 2>&1', ANPATH_BASE, PROCESSOR_PATH, $context->entity->id);
+            // Use this for testing only
+            // $phpPath = '/Applications/MAMP/bin/php/php7.3.24/bin/php';    
+            // $command = sprintf($phpPath . ' %s/index.php %s id=%d 2>&1', ANPATH_BASE, PROCESSOR_PATH, $context->entity->id);
             // error_log($command);
             // error_log(shell_exec($command));
         }
