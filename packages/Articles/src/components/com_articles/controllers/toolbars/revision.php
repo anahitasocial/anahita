88f7d6464e2a62d07d6121cc73372d93e9a1c277
rmdstudio <rastin@rmdstudio.com>
2022-05-10 22:29:14 -0700

    #522 - php 7.4 - changed: cleanup

--- packages/Articles/src/components/com_articles/controllers/toolbars/revision.php
+++ packages/Articles/src/components/com_articles/controllers/toolbars/revision.php
@@ -5,7 +5,6 @@
  *
  * @category   Anahita
  *
- * @author     Arash Sanieyan <ash@anahitapolis.com>
  * @author     Rastin Mehr <rastin@anahita.io>
  * @license    GNU GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>
  *
