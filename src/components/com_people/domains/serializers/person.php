7a0ef36ad0103b934d52fb616bfb349624262f4f
rmdstudio <rastin@rmdstudio.com>
2022-09-04 21:44:21 -0700

    people - added: getDate to lastVisitDate

--- src/components/com_people/domains/serializers/person.php
+++ src/components/com_people/domains/serializers/person.php
@@ -30,7 +30,7 @@ class ComPeopleDomainSerializerPerson extends ComActorsDomainSerializerActor
             $data['email'] = $entity->email;
             $data['usertype'] = $entity->usertype;
             $data['gender'] = $entity->gender;
-            $data['lastVisitDate'] = $entity->lastVisitDate;
+            $data['lastVisitDate'] = $entity->lastVisitDate->getDate();
         }
 
         return $data;
